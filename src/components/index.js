0e793a651735f7e0ffb882bd0e327a435a1095a7
Nicolas Laval <nicolas.laval@insee.fr>
2019-02-26 16:10:18 +0100

    Add ProgressBar component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as Button } from './button';
 export { CheckboxOne, CheckboxBoolean } from './checkbox';
 export { InputNumber } from './input';
+export { default as ProgressBar } from './progress-bar';
 export { default as Sequence } from './sequence';
 export { default as Subsequence } from './subsequence';
---
+++ src/stories/progress-bar/progress-bar.stories.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withReadme } from 'storybook-readme';
+import { ProgressBar } from 'components';
+import readme from './README.md';
+import titleDecorator from 'utils/decorator/title-decorator';
+
+const stories = storiesOf('ProgressBar', module)
+	.addDecorator(withReadme(readme))
+	.addDecorator(Component => {
+		const WrappedComponent = titleDecorator(Component);
+		return <WrappedComponent title="<ProgressBar />" />;
+	});
+
+stories.addWithJSX('Default', () => (
+	<React.Fragment>
+		{Array.from(new Array(11), (a, i) => (
+			<React.Fragment key={i}>
+				<ProgressBar id={`default-${i * 10}`} value={i * 10} />
+				<br />
+			</React.Fragment>
+		))}
+	</React.Fragment>
+));
