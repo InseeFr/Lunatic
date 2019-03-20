a9a168b8edf647b35453f6d242491705eb041a2a
Nicolas Laval <nicolas.laval@insee.fr>
2019-03-20 13:49:06 +0100

    Add declarations to Sequence

--- src/components/sequence/component.js
+++ src/components/sequence/component.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Declarations from '../declarations';
+import * as C from 'utils/constants';
 import { buildStyleObject } from '../../utils/string-utils';
 import './sequence.scss';
 
-const Sequence = ({ id, label, style }) => (
-	<div
-		aria-label={`sequence-${id}`}
-		className="sequence-lunatic"
-		style={buildStyleObject(style)}
-	>
-		{label}
-	</div>
+const Sequence = ({ id, label, declarations, style }) => (
+	<React.Fragment>
+		<Declarations
+			id={id}
+			type={C.BEFORE_QUESTION_TEXT}
+			declarations={declarations}
+		/>
+		<div
+			aria-label={`sequence-${id}`}
+			className="sequence-lunatic"
+			style={buildStyleObject(style)}
+		>
+			{label}
+		</div>
+		<Declarations
+			id={id}
+			type={C.AFTER_QUESTION_TEXT}
+			declarations={declarations}
+		/>
+		<Declarations id={id} type={C.DETACHABLE} declarations={declarations} />
+	</React.Fragment>
 );
 
 Sequence.propTypes = {
