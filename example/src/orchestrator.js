cabbc13375b9bd9402fd1a007c6b2b4173b48a95
Nicolas Laval <nicolas.laval@insee.fr>
2019-07-15 15:31:43 +0200

    Update example

--- example/src/orchestrator.js
+++ example/src/orchestrator.js
@@ -4,7 +4,7 @@ import simspons from './simpsons';
 import './custom-lunatic.scss';
 
 const Orchestrator = () => {
-	const [variables, setVariables] = useState({});
+	const [variables, setVariables] = useState(lunatic.getInitialState(simspons));
 	const components = simspons.components.map(q => {
 		const { id, componentType } = q;
 		const Component = lunatic[componentType];
@@ -19,7 +19,17 @@ const Orchestrator = () => {
 			<div className="lunatic-img">
 				<img alt="lunatic" src="/images/lunatic-logo.png" />
 			</div>
-			<div className="container">{components}</div>
+			<div className="container">
+				<div className="components">{components}</div>
+				<div className="state">
+					<h1 className="centered">Collected</h1>
+					<div>
+						{Object.entries(variables.COLLECTED).map(([key, values]) => (
+							<div>{`${key} : ${values.COLLECTED}`}</div>
+						))}
+					</div>
+				</div>
+			</div>
 		</>
 	);
 };
