7e0324d1446ab7f2e3855cd792706280a311ec02
Nicolas Laval <nicolas.laval@insee.fr>
2019-07-15 14:39:32 +0200

    Add init state utils

---
+++ src/tests/utils/init-state.spec.js
@@ -0,0 +1,168 @@
+import { getInitialState } from 'utils/init-state';
+
+describe('init-state', () => {
+	describe('getInitialState', () => {
+		it('should return empty object', () => {
+			expect(getInitialState([])).toEqual({
+				COLLECTED: {},
+				CALCULATED: {},
+				EXTERNAL: {},
+			});
+		});
+		it('should return object', () => {
+			expect(getInitialState(questionnaire)).toEqual(state);
+		});
+	});
+});
+
+const questionnaire = {
+	components: [
+		{
+			componentType: 'Sequence',
+		},
+		{
+			componentType: 'Textarea',
+			response: {
+				name: 'COMMENT',
+				valueState: [
+					{ valueType: 'PREVIOUS', value: 'PREVIOUS' },
+					{ valueType: 'COLLECTED', value: 'COLLECTED' },
+					{ valueType: 'FORCED', value: 'FORCED' },
+					{ valueType: 'EDITED', value: 'EDITED' },
+					{ valueType: 'INPUTED', value: 'INPUTED' },
+				],
+			},
+		},
+		{
+			componentType: 'Checkbox',
+			options: [
+				{
+					response: {
+						name: 'PET1',
+						valueState: [
+							{ valueType: 'PREVIOUS', value: false },
+							{ valueType: 'COLLECTED', value: false },
+							{ valueType: 'FORCED', value: false },
+							{ valueType: 'EDITED', value: false },
+							{ valueType: 'INPUTED', value: false },
+						],
+					},
+				},
+				{
+					response: {
+						name: 'PET2',
+						valueState: [
+							{ valueType: 'PREVIOUS', value: false },
+							{ valueType: 'COLLECTED', value: false },
+							{ valueType: 'FORCED', value: false },
+							{ valueType: 'EDITED', value: false },
+							{ valueType: 'INPUTED', value: false },
+						],
+					},
+				},
+			],
+		},
+		{
+			componentType: 'Table',
+			cells: [
+				[
+					{ value: '1', label: 'Vanilla' },
+					{
+						componentType: 'Radio',
+						response: {
+							name: 'ICE_FLAVOUR1',
+							valueState: [
+								{ valueType: 'PREVIOUS', value: '' },
+								{ valueType: 'COLLECTED', value: '' },
+								{ valueType: 'FORCED', value: '' },
+								{ valueType: 'EDITED', value: '' },
+								{ valueType: 'INPUTED', value: '' },
+							],
+						},
+					},
+				],
+				[
+					{ value: '2', label: 'Other' },
+					{
+						componentType: 'Checkbox',
+						options: [
+							{
+								response: {
+									name: 'ICE_FLAVOUR21',
+									valueState: [
+										{ valueType: 'PREVIOUS', value: false },
+										{ valueType: 'COLLECTED', value: false },
+										{ valueType: 'FORCED', value: false },
+										{ valueType: 'EDITED', value: false },
+										{ valueType: 'INPUTED', value: false },
+									],
+								},
+							},
+							{
+								response: {
+									name: 'ICE_FLAVOUR22',
+									valueState: [
+										{ valueType: 'PREVIOUS', value: true },
+										{ valueType: 'COLLECTED', value: true },
+										{ valueType: 'FORCED', value: true },
+										{ valueType: 'EDITED', value: true },
+										{ valueType: 'INPUTED', value: true },
+									],
+								},
+							},
+						],
+					},
+				],
+			],
+		},
+	],
+	variables: [],
+};
+const state = {
+	COLLECTED: {
+		COMMENT: {
+			PREVIOUS: 'PREVIOUS',
+			COLLECTED: 'COLLECTED',
+			FORCED: 'FORCED',
+			EDITED: 'EDITED',
+			INPUTED: 'INPUTED',
+		},
+		PET1: {
+			PREVIOUS: false,
+			COLLECTED: false,
+			FORCED: false,
+			EDITED: false,
+			INPUTED: false,
+		},
+		PET2: {
+			PREVIOUS: false,
+			COLLECTED: false,
+			FORCED: false,
+			EDITED: false,
+			INPUTED: false,
+		},
+		ICE_FLAVOUR1: {
+			PREVIOUS: '',
+			COLLECTED: '',
+			FORCED: '',
+			EDITED: '',
+			INPUTED: '',
+		},
+		ICE_FLAVOUR21: {
+			PREVIOUS: false,
+			COLLECTED: false,
+			FORCED: false,
+			EDITED: false,
+			INPUTED: false,
+		},
+		ICE_FLAVOUR22: {
+			PREVIOUS: true,
+			COLLECTED: true,
+			FORCED: true,
+			EDITED: true,
+			INPUTED: true,
+		},
+	},
+	CALCULATED: {},
+	EXTERNAL: {},
+};
---
+++ src/utils/init-state.js
@@ -0,0 +1,51 @@
+export const getInitialState = questionnaire => {
+	const { components, variables } = questionnaire;
+	const COLLECTED = getVariablesFromComponents(components);
+	//	TODO
+	// const CALCULATED = Array.isArray(variables)
+	// 	? variables.filter(({ variableType }) => variableType === 'CALCULATED')
+	// 	: {};
+	// const EXTERNAL = Array.isArray(variables)
+	// 	? variables.filter(({ variableType }) => variableType === 'EXTERNAL')
+	// 	: {};
+	return { COLLECTED, CALCULATED: {}, EXTERNAL: {} };
+};
+
+const getVariablesFromComponents = components =>
+	Array.isArray(components)
+		? components.reduce((_, { componentType, response, options, cells }) => {
+				if (
+					!componentType ||
+					['Sequence', 'Subsequence'].includes(componentType)
+				)
+					return _;
+				else if (componentType === 'Checkbox') {
+					return { ..._, ...getVariableFromOptions(options) };
+				} else if (componentType === 'Table') {
+					return { ..._, ...getVariableFromCells(cells) };
+				} else {
+					return { ..._, ...getVariableFromResponse(response) };
+				}
+		  }, {})
+		: {};
+
+const getVariableFromResponse = response => {
+	const { name, valueState } = response;
+	const values = valueState.reduce(
+		(__, { valueType, value }) => ({ ...__, [valueType]: value }),
+		{}
+	);
+	return { [name]: values };
+};
+
+const getVariableFromOptions = options =>
+	options.reduce(
+		(_, { response }) => ({ ..._, ...getVariableFromResponse(response) }),
+		{}
+	);
+
+const getVariableFromCells = cells =>
+	cells.reduce(
+		(_, components) => ({ ..._, ...getVariablesFromComponents(components) }),
+		{}
+	);
