f6e99ff075df764e3df188c3d67d4e547b67ab26
travis <>
2019-07-16 09:51:30 +0000

    Rebuild doc for revision 74d9b8943e5aa0cc2376fd4fc9c30f5392cd0fb8: Update deploy.sh

---
+++ precache-manifest.e578f36f903892ace750cd5d8016f7e6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e006c34208c708f6f23b071201a31733",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a2ba977150e80c308077",
+    "url": "./static/css/main.da69d432.chunk.css"
+  },
+  {
+    "revision": "4fece24ce9c20208a90c",
+    "url": "./static/js/2.9235b295.chunk.js"
+  },
+  {
+    "revision": "a2ba977150e80c308077",
+    "url": "./static/js/main.600fc29a.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "f824e79fb08f78983cc9f2fcc46c7337",
+    "url": "./static/media/check_box.f824e79f.svg"
+  },
+  {
+    "revision": "b05d3d9754f2c4758b70002ab5ece7e1",
+    "url": "./static/media/check_box_outline.b05d3d97.svg"
+  },
+  {
+    "revision": "f2d2cbf35cb7d6a4e4fcca8afdc8bb31",
+    "url": "./static/media/lunatic-logo.f2d2cbf3.png"
+  },
+  {
+    "revision": "1e6f7d2a31a9a2791e9f096a23189984",
+    "url": "./static/media/menu-down.1e6f7d2a.svg"
+  },
+  {
+    "revision": "bb5d796fbf774bd1080ba3bce45834ef",
+    "url": "./static/media/menu-up.bb5d796f.svg"
+  },
+  {
+    "revision": "c6b92d4b4cb3544057f2e18f11cb92dc",
+    "url": "./static/media/radio_button_checked.c6b92d4b.svg"
+  },
+  {
+    "revision": "6a19a534a0e089b2fb8e9d8263387d70",
+    "url": "./static/media/radio_button_unchecked.6a19a534.svg"
+  }
+]);
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Orchestrator from './orchestrator';
-import './index.scss';
-
-ReactDOM.render(<Orchestrator />, document.getElementById('root'));
