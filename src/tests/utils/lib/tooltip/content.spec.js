e1268d40242fcf6f663099c33918d0edac2a3c01
Nicolas Laval <nicolas.laval@insee.fr>
2019-07-29 22:23:25 +0200

    Add tooltip content test

---
+++ src/tests/utils/lib/tooltip/content.spec.js
@@ -0,0 +1,89 @@
+import { buildTooltip } from 'utils/lib';
+
+describe('build tooltip content', () => {
+	describe('buildTooltip', () => {
+		it('should return default value', () => {
+			expect(buildTooltip()).toEqual({});
+			expect(buildTooltip({ valueState: [] })).toEqual({});
+			expect(
+				buildTooltip({
+					valueState: [{ valueType: 'COLLECTED', value: 'Collected' }],
+				})
+			).toEqual({});
+		});
+		it('should return COLLECTED', () => {
+			expect(
+				buildTooltip({
+					valueState: [
+						{ valueType: 'COLLECTED', value: 'Collected' },
+						{ valueType: 'FORCED', value: 'Forced' },
+					],
+				})
+			).toEqual({
+				content: [
+					{
+						key: 'Brute',
+						value: 'Collected',
+					},
+				],
+				imgName: 'forcedImg',
+			});
+			expect(
+				buildTooltip({
+					valueState: [
+						{ valueType: 'COLLECTED', value: null },
+						{ valueType: 'FORCED', value: 'Forced' },
+					],
+				})
+			).toEqual({
+				content: [
+					{
+						key: 'Brute',
+						value: ' - ',
+					},
+				],
+				imgName: 'forcedImg',
+			});
+		});
+		it('should return COLLECTED FORCED', () => {
+			expect(
+				buildTooltip({
+					valueState: [
+						{ valueType: 'COLLECTED', value: 'Collected' },
+						{ valueType: 'FORCED', value: 'Forced' },
+						{ valueType: 'EDITED', value: 'Edited' },
+					],
+				})
+			).toEqual({
+				content: [
+					{
+						key: 'Brute',
+						value: 'Collected',
+					},
+					{
+						key: 'Correction automatique',
+						value: 'Forced',
+					},
+				],
+				imgName: 'editedImg',
+			});
+			expect(
+				buildTooltip({
+					valueState: [
+						{ valueType: 'COLLECTED', value: null },
+						{ valueType: 'FORCED', value: null },
+						{ valueType: 'EDITED', value: 'Edited' },
+					],
+				})
+			).toEqual({
+				content: [
+					{
+						key: 'Brute',
+						value: ' - ',
+					},
+				],
+				imgName: 'editedImg',
+			});
+		});
+	});
+});
