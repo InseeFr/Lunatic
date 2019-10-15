9fdfb9d7a3726f399cb0a17874c66d755cdc92ee
Nicolas Laval <nicolas.laval@insee.fr>
2019-10-15 07:46:06 +0200

    Update Dropdown story

--- src/stories/dropdown/dropdown.stories.js
+++ src/stories/dropdown/dropdown.stories.js
@@ -1,17 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withReadme } from 'storybook-readme';
-import { Dropdown } from 'components';
+import Orchestrator from '../utils/orchestrator';
 import readme from './README.md';
 import { titleDecorator } from 'utils/lib';
-import {
-	text,
-	boolean,
-	number,
-	color,
-	object,
-	select,
-} from '@storybook/addon-knobs/react';
+import data from './data';
+import { text, boolean, select } from '@storybook/addon-knobs/react';
 
 const stories = storiesOf('Dropdown', module)
 	.addDecorator(withReadme(readme))
@@ -19,15 +13,6 @@ const stories = storiesOf('Dropdown', module)
 		const WrappedComponent = titleDecorator(Component);
 		return <WrappedComponent title="<Dropdown />" />;
 	});
-
-const options = [
-	{ value: 'belgium', label: 'Belgium' },
-	{ value: 'france', label: 'France' },
-	{ value: 'italy', label: 'Italy' },
-	{ value: 'netherland', label: 'Netherland' },
-	{ value: 'spain', label: 'Spain' },
-	{ value: 'sweden', label: 'Sweden' },
-];
 
 const labelPositionOptions = {
 	DEFAULT: 'Default',
@@ -37,33 +22,19 @@ const labelPositionOptions = {
 	LEFT: 'Left',
 };
 
-const defaultProps = {
-	handleChange: console.log,
-	response: {
-		name: 'DROPDOWN',
-		valueState: [
-			{ valueType: 'COLLECTED', value: 'belgium' },
-			{ valueType: 'FORCED', value: 'italy' },
-		],
-	},
-	options,
-	labelPositionOptions,
-};
-
 stories.addWithJSX('Default', () => (
-	<Dropdown id="default" label="Default dropdown" {...defaultProps} />
+	<Orchestrator id="default" source={data} />
 ));
 
 stories.addWithJSX('Props', () => (
-	<Dropdown
-		{...defaultProps}
+	<Orchestrator
 		id="props"
+		source={data}
 		label={text('Label', "I'm the label of the dropdown")}
-		options={options}
 		placeholder={text('Placeholder', 'Placeholder')}
 		readOnly={boolean('Read only', false)}
 		writable={boolean('Writable', false)}
-		labelPosition={select('Label position', labelPositionOptions)}
+		labelPosition={select('Label position', labelPositionOptions, 'DEFAULT')}
 		preferences={['COLLECTED', 'FORCED']}
 		tooltip={boolean('Tooltip', false)}
 	/>
