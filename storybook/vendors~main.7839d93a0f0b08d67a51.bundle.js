5755dc4cfbaddf0bf8b0a333faee3b87235c8361
travis <>
2019-11-07 17:57:10 +0000

    Rebuild doc for revision 0d0742d3c52788790ab18e06563edc35b638723d: Add props to Table component (Fix #9)

--- storybook/vendors~main.7839d93a0f0b08d67a51.bundle.js
+++ storybook/vendors~main.7839d93a0f0b08d67a51.bundle.js
@@ -114,4 +114,4 @@ function isObjectObject(o){return!0===
  * Released under the MIT License.
  */
 function isObject(val){return null!=val&&"object"==typeof val&&!1===Array.isArray(val)}(o)&&"[object Object]"===Object.prototype.toString.call(o)}function isPlainObject(o){var ctor,prot;return!1!==isObjectObject(o)&&("function"==typeof(ctor=o.constructor)&&(!1!==isObjectObject(prot=ctor.prototype)&&!1!==prot.hasOwnProperty("isPrototypeOf")))}__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,"default",(function(){return isPlainObject}))}]]);
-//# sourceMappingURL=vendors~main.ebd91fe31a909967e664.bundle.js.map
+//# sourceMappingURL=vendors~main.7839d93a0f0b08d67a51.bundle.js.map
