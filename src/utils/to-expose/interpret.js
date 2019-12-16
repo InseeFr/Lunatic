d3adbfacfe3beeffdcd939696073b4f29a5f3ce6
Nicolas Laval <nicolas.laval@insee.fr>
2019-12-16 08:08:01 +0100

    Update interpret function

--- src/utils/to-expose/interpret.js
+++ src/utils/to-expose/interpret.js
@@ -3,11 +3,14 @@ import { interpret as interpretVtl } from '@inseefr/vtl-tools';
 export const interpret = features => bindings => expression => {
 	if (!expression) return '';
 	if (!Array.isArray(features)) return expression;
+
 	if (features.includes('VTL')) {
 		try {
-			return interpretVtl(expression, bindings);
+			const VTLExpr = interpretVtl(expression, bindings);
+			if (!VTLExpr) return expression;
+			return VTLExpr;
 		} catch (e) {
-			return `VTL interpret error for expression: ${expression}`;
+			return expression;
 		}
 	}
 	return expression;
