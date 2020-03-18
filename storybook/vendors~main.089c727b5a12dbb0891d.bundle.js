78df3be88e3d448780c45a287c4137501fa6d996
travis <>
2020-03-18 09:24:10 +0000

    Rebuild doc for revision bcf5aa8c5ccaa5eb27abe20d008bef56e0df9793: permet la cohabitation sur le label des checkbox boolean (#32)

--- storybook/vendors~main.089c727b5a12dbb0891d.bundle.js
+++ storybook/vendors~main.089c727b5a12dbb0891d.bundle.js
@@ -113,4 +113,4 @@ function isObjectObject(o){return!0===
  * Released under the MIT License.
  */
 function isObject(val){return null!=val&&"object"==typeof val&&!1===Array.isArray(val)}(o)&&"[object Object]"===Object.prototype.toString.call(o)}function isPlainObject(o){var ctor,prot;return!1!==isObjectObject(o)&&("function"==typeof(ctor=o.constructor)&&(!1!==isObjectObject(prot=ctor.prototype)&&!1!==prot.hasOwnProperty("isPrototypeOf")))}__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,"default",(function(){return isPlainObject}))}]]);
-//# sourceMappingURL=vendors~main.204f36b3f41c7874c6cc.bundle.js.map
+//# sourceMappingURL=vendors~main.089c727b5a12dbb0891d.bundle.js.map
