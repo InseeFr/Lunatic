74bf7ca77c4ce0c75ffe8f20ed34d48271e2fdbd
Nicolas Laval <nicolas.laval@insee.fr>
2020-03-23 16:40:36 +0100

    Add InputDeclarationsWrapper component

--- src/components/declarations/wrappers/index.js
+++ src/components/declarations/wrappers/index.js
@@ -1,2 +1,3 @@
-export { default as SimpleDeclarationsWrapper } from './simple-declarations-wrapper';
+export { default as InputDeclarationsWrapper } from './input-declarations-wrapper';
 export { default as ListDeclarationsWrapper } from './list-declarations-wrapper';
+export { default as SimpleDeclarationsWrapper } from './simple-declarations-wrapper';
