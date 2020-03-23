ca920a6ef6885f9478cc4383fe566dfba86d24ac
Nicolas Laval <nicolas.laval@insee.fr>
2020-03-23 12:12:26 +0100

    Fix test issue

---
+++ src/tests/components/declarations-wrappers/list-declarations-wrapper.spec.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { ListDeclarationsWrapper } from 'components/declarations/wrappers';
+
+const handleChange = jest.fn();
+const options = [
+	{ value: 'france', label: 'France' },
+	{ value: 'italy', label: 'Italy' },
+];
+const response = { valueState: [{ valueType: 'COLLECTED', value: 'italy' }] };
+
+const defaultProps = {
+	id: 'id',
+	label: 'label',
+	handleChange,
+	options,
+	response,
+};
+
+describe('list-declarations-wrapper', () => {
+	it('renders without crashing', () => {
+		mount(<ListDeclarationsWrapper {...defaultProps} type="radio" />);
+		mount(<ListDeclarationsWrapper {...defaultProps} type="checkbox" />);
+	});
+
+	it('returns tooltip and keyboardSelection component', () => {
+		shallow(
+			<ListDeclarationsWrapper
+				{...defaultProps}
+				tooltip
+				keyboardSelection
+				type="radio"
+			/>
+		);
+	});
+
+	it('renders firing useEffect', () => {
+		const wrapper = mount(
+			<ListDeclarationsWrapper {...defaultProps} type="radio" />
+		);
+		wrapper.setProps({ focused: true });
+	});
+
+	it('should trigger the change event', () => {
+		const wrapper = shallow(
+			<ListDeclarationsWrapper {...defaultProps} type="radio" />
+		);
+		wrapper
+			.find('input')
+			.first()
+			.simulate('change');
+		expect(handleChange).toHaveBeenCalled();
+		expect(handleChange).toHaveBeenCalledWith({ '': 'france' });
+	});
+});
