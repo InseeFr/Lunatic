e6f8ccc6d99365c4bae6ee076323b887ad47c31f
NicoLaval <nicolas.laval@insee.fr>
2020-05-27 17:00:28 +0200

    Fix minor issue

--- src/utils/lib/memo-check.js
+++ src/utils/lib/memo-check.js
@@ -1,8 +1,6 @@
 import isEqual from 'lodash.isequal';
 
 export const areEqual = (prevProps, props) => {
-	console.log(props);
-	console.log(isEqual(prevProps.response, props.response));
 	if (props.bindingsDependency) return false;
 	if (props.response) return isEqual(prevProps.response, props.response);
 	if (props.responses) return isEqual(prevProps.responses, props.responses);
