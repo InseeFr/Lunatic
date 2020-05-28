4b93627fa14cf528dcfca8189639a91438635f3d
NicoLaval <nicolas.laval@insee.fr>
2020-05-28 15:10:00 +0200

    Upgrade vtl tools

--- src/utils/to-expose/interpret.js
+++ src/utils/to-expose/interpret.js
@@ -1,12 +1,12 @@
-import { interpret as interpretVtl } from '@inseefr/vtl-tools';
+import { interpret as interpretVtl } from '@inseefr/vtl-2.1-engine';
 
-export const interpret = features => bindings => expression => {
+export const interpret = (features) => (bindings) => (expression) => {
 	if (!expression) return '';
 	if (!Array.isArray(features)) return expression;
 	if (features.includes('VTL')) {
 		try {
-			const replacedBindings = replaceNullBindings(bindings);
-			const VTLExpr = interpretVtl(expression, replacedBindings);
+			//const replacedBindings = replaceNullBindings(bindings);
+			const VTLExpr = interpretVtl(expression, bindings);
 			if (!VTLExpr) return expression;
 			return VTLExpr;
 		} catch (e) {
@@ -16,7 +16,7 @@ export const interpret = features => bindings => expression => {
 	return expression;
 };
 
-export const replaceNullBindings = bindings =>
+export const replaceNullBindings = (bindings) =>
 	bindings
 		? Object.entries(bindings).reduce(
 				(acc, [key, value]) => ({
