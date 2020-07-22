16163a1062e8850d2e91e9861eccbe49e3646db6
NicoLaval <nicolas.laval@insee.fr>
2020-07-22 20:38:32 +0200

    Update memo conditionnal function

--- src/utils/lib/memo-check.js
+++ src/utils/lib/memo-check.js
@@ -1,10 +1,24 @@
 import isEqual from 'lodash.isequal';
 
-// If areEqual is call, bindings have necessarily changed
 export const areEqual = (prevProps, props) => {
-	if (props.bindingsDependency) return false;
-	if (props.response) return isEqual(prevProps.response, props.response);
-	if (props.responses) return isEqual(prevProps.responses, props.responses);
-	if (props.cells) return isEqual(prevProps.cells, props.cells);
-	return true;
+	const { bindingsDependency } = props;
+	if (Array.isArray(bindingsDependency) && bindingsDependency.length > 0) {
+		const { bindings: oldB } = prevProps;
+		const { bindings: newB } = props;
+		return bindingsDependency.reduce(
+			(acc, dep) => (acc ? acc : isEqual(oldB[dep], newB[dep])),
+			false
+		);
+	}
+	return VarsHasBeenUpdated(prevProps, props);
+};
+
+const VarsHasBeenUpdated = (prevProps, props) => {
+	const { response: oldR, responses: oldRs, cells: oldC } = prevProps;
+	const { response: newR, responses: newRs, cells: newC } = props;
+	return (
+		(newR && isEqual(oldR, newR)) ||
+		(newRs && isEqual(oldRs, newRs)) ||
+		(newC && isEqual(oldC, newC))
+	);
 };
