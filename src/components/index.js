12896df20c9d431386bb27bff03c418875640d6d
NicoLaval <nicolas.laval@insee.fr>
2020-07-29 09:27:19 +0200

    Move RosterForLoop export location

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export * from './components';
 // Avoid circular deps
 export { default as Table } from './table';
-export { RosterForLoop, Loop } from './loops';
+export { Loop } from './loops';
 export * from '../utils/to-expose';
