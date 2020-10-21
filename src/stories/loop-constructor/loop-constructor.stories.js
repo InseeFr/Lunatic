e35627cf79b1314299710deeb8d10700c33d20d4
NicoLaval <nicolas.laval@insee.fr>
2020-10-21 17:23:48 +0200

    Add stories for BlockConstructor component

--- src/stories/loop-constructor/loop-constructor.stories.js
+++ src/stories/loop-constructor/loop-constructor.stories.js
@@ -5,6 +5,7 @@ import Orchestrator from '../utils/orchestrator';
 import readme from './README.md';
 import { titleDecorator } from 'utils/lib';
 import dataRoster from './data-roster';
+import dataBlock from './data-roster';
 import { positioningOptions } from '../utils/options';
 import { select } from '@storybook/addon-knobs/react';
 
@@ -19,6 +20,22 @@ storiesRoster.addWithJSX('Default', () => (
 	<Orchestrator
 		id="default"
 		source={dataRoster}
+		positioning={select('Items positioning', positioningOptions, 'DEFAULT')}
+		features={['VTL']}
+	/>
+));
+
+const storiesBlock = storiesOf('LoopConstructor/BlockForLoop', module)
+	.addDecorator(withReadme(readme))
+	.addDecorator((Component) => {
+		const WrappedComponent = titleDecorator(Component);
+		return <WrappedComponent title="BlockForLoop" />;
+	});
+
+storiesBlock.addWithJSX('Default', () => (
+	<Orchestrator
+		id="default"
+		source={dataBlock}
 		positioning={select('Items positioning', positioningOptions, 'DEFAULT')}
 		features={['VTL']}
 	/>
