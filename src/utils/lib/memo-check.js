b6f85ead61fb285214ce89b0c251dc206673d484
NicoLaval <nicolas.laval@insee.fr>
2020-11-09 08:56:23 +0100

    Update memo-check.js

--- src/utils/lib/memo-check.js
+++ src/utils/lib/memo-check.js
@@ -1,6 +1,17 @@
 import isEqual from 'lodash.isequal';
 
+const VarsHasBeenUpdated = (prevProps, props) => {
+	const { response: oldR, responses: oldRs, cells: oldC } = prevProps;
+	const { response: newR, responses: newRs, cells: newC } = props;
+	if (!newR && !newRs && !newC) return true;
+	return (
+		!(newR && isEqual(oldR, newR)) ||
+		(newRs && isEqual(oldRs, newRs)) ||
+		(newC && isEqual(oldC, newC))
+	);
+};
 export const areEqual = (prevProps, props) => {
+	if (VarsHasBeenUpdated(prevProps, props)) return false;
 	const { bindingDependencies } = props;
 	if (Array.isArray(bindingDependencies) && bindingDependencies.length > 0) {
 		const { bindings: oldB } = prevProps;
@@ -9,16 +20,5 @@ export const areEqual = (prevProps, props) => {
 			.map((d) => isEqual(oldB[d], newB[d]))
 			.includes(false);
 	}
-	return VarsHasBeenUpdated(prevProps, props);
-};
-
-const VarsHasBeenUpdated = (prevProps, props) => {
-	const { response: oldR, responses: oldRs, cells: oldC } = prevProps;
-	const { response: newR, responses: newRs, cells: newC } = props;
-	if (!newR && !newRs && !newC) return true;
-	return (
-		(newR && isEqual(oldR, newR)) ||
-		(newRs && isEqual(oldRs, newRs)) ||
-		(newC && isEqual(oldC, newC))
-	);
+	return true;
 };
