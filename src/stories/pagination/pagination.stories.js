2ae5cdb3925e15c53b38d0f00ab09c6464f912d9
NicoLaval <nicolas.laval@insee.fr>
2021-03-18 10:50:20 +0100

    Add pagination simpsons stories

--- src/stories/pagination/pagination.stories.js
+++ src/stories/pagination/pagination.stories.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Orchestrator from '../utils/orchestrator';
 import simpleLoop from './simple-loop';
+import simpsonsQuestion from './simpsons-question.json';
+import simpsonsSequence from './simpsons-sequence.json';
 import { titleDecorator } from 'utils/lib';
 
 const stories = storiesOf('Pagination', module).addDecorator((Component) => {
@@ -13,6 +15,31 @@ stories.addWithJSX('SimpleLoop', () => (
 	<Orchestrator
 		id="simple-loop"
 		source={simpleLoop}
+		features={['VTL']}
+		pagination
+	/>
+));
+
+const storiesSimpsons = storiesOf('Pagination/Simpsons', module).addDecorator(
+	(Component) => {
+		const WrappedComponent = titleDecorator(Component);
+		return <WrappedComponent title="<Pagination />" />;
+	}
+);
+
+storiesSimpsons.addWithJSX('Question', () => (
+	<Orchestrator
+		id="simple-loop"
+		source={simpsonsQuestion}
+		features={['VTL']}
+		pagination
+	/>
+));
+
+storiesSimpsons.addWithJSX('Sequence', () => (
+	<Orchestrator
+		id="simple-loop"
+		source={simpsonsSequence}
 		features={['VTL']}
 		pagination
 	/>
