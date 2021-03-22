fb47c1aee99a3a8ca1569fe9d02921ec7277f392
NicoLaval <nicolas.laval@insee.fr>
2021-03-22 09:44:08 +0100

    Remove collected img for tooltip

--- src/components/tooltip/img/index.js
+++ src/components/tooltip/img/index.js
@@ -1,5 +1,4 @@
-import collectedImg from './collected.png';
 import editedImg from './edited.png';
 import forcedImg from './forced.png';
 
-export { collectedImg, editedImg, forcedImg };
+export { editedImg, forcedImg };
