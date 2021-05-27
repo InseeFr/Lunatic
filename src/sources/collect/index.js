467c0d10f5d13ac2a404121d40dd2fe3836c1cfc
NicoLaval <NicoLaval@users.noreply.github.com>
2021-05-27 10:11:04 +0000

    deploy: daad0a6fbef209156435415139d6a98b1ea38086

--- src/sources/collect/index.js
+++
@@ -1,3 +0,0 @@
-export { default as simpsonsCollect } from './simpsons';
-export { default as simpsonsSequenceCollect } from './simpsons-sequence';
-export { default as simpsonsQuestionCollect } from './simpsons-question';
--- src/sources/management/index.js
+++
@@ -1,3 +0,0 @@
-export { default as simpsonsManagement } from './simpsons';
-export { default as simpsonsSequenceManagement } from './simpsons-sequence';
-export { default as simpsonsQuestionManagement } from './simpsons-question';
