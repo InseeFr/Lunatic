a2a180c9252cc9f78a5856684350037e90a4262a
NicoLaval <nicolas.laval@insee.fr>
2021-06-03 19:54:11 +0200

    Add global cache for filters

--- src/utils/to-expose/hooks/filter-components.js
+++ src/utils/to-expose/hooks/filter-components.js
@@ -1,20 +1,42 @@
 import { interpret } from '../interpret';
 import { isDev } from '../../lib';
 
+let cache = {};
+
 const customFilterPagination = ({ page }, pagination, currentPage) => {
 	return pagination ? currentPage.split('.')[0] === page : true;
 };
 
-const filterComponents = ({
+const filterComponents = ({ components, updatedVars, features, bindings }) => {
+	const localCache = {};
+	const filtered = components.filter(({ conditionFilter }) => {
+		if (!conditionFilter || !conditionFilter.value) return true;
+		const { bindingDependencies, value } = conditionFilter;
+		if (localCache[value] !== undefined) return localCache[value];
+		if (
+			(!bindingDependencies ||
+				!updatedVars.some((t) => bindingDependencies.includes(t))) &&
+			cache[value] !== undefined
+		)
+			return cache[value];
+		const inter = interpret(features)(bindings)(value);
+		localCache[value] = inter;
+		return inter;
+	});
+	cache = { ...cache, ...localCache };
+	return filtered;
+};
+
+const buildComponents = ({
 	components,
 	management,
 	bindings,
 	features,
 	page,
 	pagination,
+	todo,
 }) => {
-	const cache = {};
-
+	console.log(cache);
 	if (isDev) {
 		console.log('Start filter');
 		var start = new Date().getTime();
@@ -27,14 +49,14 @@ const filterComponents = ({
 			customFilterPagination(c, pagination, page)
 		);
 
+	const updatedVars = Object.keys(todo);
+
 	if (!pagination) {
-		const filtered = components.filter(({ conditionFilter }) => {
-			if (!conditionFilter || !conditionFilter.value) return true;
-			const { value } = conditionFilter;
-			if (cache[value] !== undefined) return cache[value];
-			const inter = interpret(features)(bindings)(value);
-			cache[value] = inter;
-			return inter;
+		const filtered = filterComponents({
+			components,
+			updatedVars,
+			features,
+			bindings,
 		});
 		if (isDev) console.log(`End filter: ${new Date().getTime() - start} ms`);
 		return filtered;
@@ -44,21 +66,18 @@ const filterComponents = ({
 		customFilterPagination(c, pagination, page)
 	);
 
-	const pageComponentsFiltered = pageComponents.filter(
-		({ conditionFilter }) => {
-			if (!conditionFilter || !conditionFilter.value) return true;
-			const { value } = conditionFilter;
-			if (cache[value] !== undefined) return cache[value];
-			const inter = interpret(features)(bindings)(value);
-			cache[value] = inter;
-			return inter;
-		}
-	);
+	const pageComponentsFiltered = filterComponents({
+		components: pageComponents,
+		updatedVars,
+		features,
+		bindings,
+	});
 	if (isDev) console.log(`End filter: ${new Date().getTime() - start}`);
 	return pageComponentsFiltered;
 };
 
 let oldComponents = [];
+let memoryTodo = {};
 
 export const useFilterComponents = ({
 	questionnaire,
@@ -69,14 +88,18 @@ export const useFilterComponents = ({
 	pagination,
 	todo,
 }) => {
-	if (Object.keys(todo).length > 0) return oldComponents;
-	const components = filterComponents({
+	if (Object.keys(todo).length > 0) {
+		memoryTodo = todo;
+		return oldComponents;
+	}
+	const components = buildComponents({
 		components: questionnaire.components,
 		management,
 		bindings,
 		features,
 		page,
 		pagination,
+		todo: memoryTodo,
 	});
 	oldComponents = components;
 	return components;
