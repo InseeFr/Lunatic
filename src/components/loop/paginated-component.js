dcdcb7f104b194d95ac47f34324d3d890f486815
NicoLaval <nicolas.laval@insee.fr>
2021-06-04 11:42:04 +0200

    Improve loop perfs

--- src/components/loop/paginated-component.js
+++ src/components/loop/paginated-component.js
@@ -82,10 +82,6 @@ const PaginatedLoop = ({
 
 	const flattenComponents = U.buildLoopComponents(iterationNb)(components);
 
-	// if (!U.displayLoop(loopDependencies)(bindings)) {
-	// 	return <div>Pas de questionnaire individuel, passez à la suite</div>;
-	// }
-
 	const {
 		currentRootPage,
 		currentComponentIndex,
@@ -143,18 +139,49 @@ const PaginatedLoop = ({
 		}
 	}
 
+	const localCache = {};
+
 	const loopComponents = flattenComponents.reduce(
 		(
 			acc,
-			{ componentType, id: idC, rowNumber, conditionFilter, page, ...rest }
+			{
+				componentType,
+				id: idC,
+				rowNumber,
+				conditionFilter,
+				page,
+				bindingDependencies = [],
+				...rest
+			}
 		) => {
-			const loopBindings =
-				U.buildBindingsForDeeperComponents(rowNumber)(bindings);
-			if (!U.displayLoopQuestion(loopDependencies)(loopBindings)) return acc;
+			const loopVars = bindingDependencies.map((b) => {
+				if (bindings[b]) return [b, bindings[b]];
+				return [b, null];
+			});
+			const loopBindings = U.buildLoopBindings(rowNumber)(loopVars);
+
+			if (!U.displayLoopQuestion(bindingDependencies)(bindings)) return acc;
 			const Component = lunatic[componentType];
 
+			const { bindingDependencies: filterDependencies = [], value = '' } =
+				conditionFilter;
+
+			const filterVars = filterDependencies.map((b) => {
+				if (bindings[b]) return [b, bindings[b]];
+				return [b, null];
+			});
+			const filterBindings = U.buildLoopBindings(rowNumber)(filterVars);
+
+			const set = () => {
+				const v = interpret(featuresWithoutMD)(filterBindings)(value);
+				localCache[value] = v;
+				return v;
+			};
+
+			const filterResult = localCache[value] || set();
+
 			if (
-				interpret(featuresWithoutMD)(loopBindings)(conditionFilter?.value) ||
+				filterResult ||
 				(pagination && !currentPageWithoutAnyIteration.startsWith(page)) ||
 				(paginatedLoop && rowNumber + 1 !== currentIteration)
 			)
@@ -168,6 +195,7 @@ const PaginatedLoop = ({
 						id={`${idC}-loop-${rowNumber}`}
 						handleChange={(up) => setTodo({ up, rowNumber })}
 						bindings={loopBindings}
+						bindingDependencies={bindingDependencies}
 						componentType={componentType}
 						pagination={pagination}
 						currentPage={currentPage}
