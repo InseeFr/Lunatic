daff7869f1c065eda495bd9bafb80e80ab17d564
Renaud Genevois <KQHLZ2@ad.insee.intra>
2021-07-07 15:34:51 +0200

    remove unuse function

--- src/utils/idb-tools/index.js
+++ src/utils/idb-tools/index.js
@@ -1,8 +1,3 @@
 export { default as openDb } from './open-db';
-export {
-	default as idbBulkInsert,
-	BULK_INSERT_MESSAGES,
-} from './idb-bulk-insert';
 export { default as clearDb } from './clear-store';
-export { default as getEntity } from './get-entity';
 export { default as getIDB } from './get-idb';
