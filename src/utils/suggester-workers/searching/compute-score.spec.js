7066ab36ae775b4fb0fc26e31e68ff96a5d9523e
Renaud Genevois <KQHLZ2@ad.insee.intra>
2021-09-16 16:22:09 +0200

    test searching with one assertion

--- src/utils/suggester-workers/searching/compute-score.spec.js
+++ src/utils/suggester-workers/searching/compute-score.spec.js
@@ -2,5 +2,8 @@ const e1 = { id: 1, text: 'toto tata titi' };
 const e2 = { id: 2, text: 'toto   tutu' };
 
 describe('', function () {
-	iterator('', function () {});
+	iterator('', function () {
+		// TODO implementrs it
+		expect(true).toBe(true);
+	});
 });
