88987c8cbcb6b73f11f9ef2035fbcb45b7b79be0
Renaud Genevois <KQHLZ2@ad.insee.intra>
2021-09-16 16:24:54 +0200

    remove bad test

--- src/utils/suggester-workers/searching/compute-score.spec.js
+++
@@ -1,9 +0,0 @@
-const e1 = { id: 1, text: 'toto tata titi' };
-const e2 = { id: 2, text: 'toto   tutu' };
-
-describe('', function () {
-	iterator('', function () {
-		// TODO implementrs it
-		expect(true).toBe(true);
-	});
-});
