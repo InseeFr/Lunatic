2789eb9f03a8a9ac08089def1c70d146bb8a19ff
NicoLaval <nicolas.laval@insee.fr>
2021-09-21 12:03:24 +0200

    Remove console.log

--- src/components/missing-wrapper/wrapper.js
+++ src/components/missing-wrapper/wrapper.js
@@ -3,7 +3,6 @@ import Missing from './component';
 
 const missingWrapper = (Component) => (props) => {
 	const { missing } = props;
-	console.log(props);
 	if (missing) return <Missing Component={Component} props={props} />;
 	return <Component {...props} />;
 };
