8920dafe4c2db7231bcc9b1e3268368d30459aac
Renaud Genevois <KQHLZ2@ad.insee.intra>
2021-09-21 09:48:42 +0200

    descending order

--- src/utils/suggester-workers/searching/order/index.js
+++ src/utils/suggester-workers/searching/order/index.js
@@ -1,22 +1,6 @@
+import createAlphanumericOrderer from './create-alphanumeric-orderer';
+
 function identity(response) {
-	return response;
-}
-
-function ascending(response, order = {}) {
-	const { field } = order;
-
-	if (field && Array.isArray(response)) {
-		return response.sort(function (a, b) {
-			if (field in a.suggestion && field in b.suggestion) {
-				if (a.suggestion[field] > b.suggestion[field]) {
-					return 1;
-				} else if (a.suggestion[field] < b.suggestion[field]) {
-					return -1;
-				}
-			}
-			return 0;
-		});
-	}
 	return response;
 }
 
@@ -24,7 +8,9 @@ function getOrderingFunction(order = {}) {
 	const { type } = order;
 	switch (type) {
 		case 'ascending':
-			return ascending;
+			return createAlphanumericOrderer(false);
+		case 'descending':
+			return createAlphanumericOrderer(true);
 		default:
 			return identity;
 	}
