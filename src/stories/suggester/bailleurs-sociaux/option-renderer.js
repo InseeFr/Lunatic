06998fd520e77e30527096410d47422f2a0c0cd6
renaud23 <renaud.genevois23@gmail.com>
2021-09-24 11:28:17 +0200

    bailleur style

--- src/stories/suggester/bailleurs-sociaux/option-renderer.js
+++ src/stories/suggester/bailleurs-sociaux/option-renderer.js
@@ -2,16 +2,15 @@ import React from 'react';
 import classnames from 'classnames';
 import './theme.scss';
 
-function OptionNafRenderer({ option, selected }) {
-	const { label, code, niveau } = option;
+function OptionBailleurRenderer({ option, selected }) {
+	const { label, typorg } = option;
 	return (
-		<div className={classnames('naf-option', { selected })}>
-			<span className={classnames('code', niveau)} title={`${niveau} ${code}`}>
-				{code}
-			</span>
-			<span className="label">{label}</span>
+		<div
+			className={classnames('option-bailleur', `type-${typorg}`, { selected })}
+		>
+			<span className="bailleur-label">{label}</span>
 		</div>
 	);
 }
 
-export default OptionNafRenderer;
+export default OptionBailleurRenderer;
