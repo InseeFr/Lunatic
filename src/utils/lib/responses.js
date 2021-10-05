1cde846980b5db505fc9834acd6bfd282e99f2f8
NicoLaval <nicolas.laval@insee.fr>
2021-10-05 10:03:26 +0200

    Fix getResponseByPreference issue

--- src/utils/lib/responses.js
+++ src/utils/lib/responses.js
@@ -2,10 +2,8 @@ export const getResponseName = (response) => (response && response.name) || '';
 
 export const getResponseByPreference = (preferences) => (response) => {
 	if (!(response && response.values)) return null;
-	return preferences.reduce((current, p) => {
-		if (p in response.values) {
-			return response.values[p];
-		}
-		return current;
+	return preferences.reduce((acc, p) => {
+		const value = response.values[p];
+		return value !== null ? value : acc;
 	}, null);
 };
