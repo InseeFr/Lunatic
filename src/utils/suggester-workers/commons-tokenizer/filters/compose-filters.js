62541237dc99cd721baa9cb6b04dfd6348d16620
Renaud Genevois <KQHLZ2@ad.insee.intra>
2021-11-04 14:15:46 +0100

    remove double

--- src/utils/suggester-workers/commons-tokenizer/filters/compose-filters.js
+++ src/utils/suggester-workers/commons-tokenizer/filters/compose-filters.js
@@ -2,9 +2,11 @@ import filterStemmer from './filter-stemmer';
 import filterLength from './filter-length';
 import filterSynonyms from './filter-synonyms';
 import filterAccentsToLower from './filter-accents-to-lower';
+import filterDouble from './filter-double';
 
 function createGetFilters({ filterStopWords }) {
 	return [
+		filterDouble,
 		filterAccentsToLower,
 		filterStemmer,
 		filterSynonyms,
