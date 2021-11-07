c8b930e244ea91c19096d1bda076fea4c852018d
NicoLaval <nicolas.laval@insee.fr>
2021-11-07 10:40:56 +0100

    Update stories

--- src/stories/questionnaire/questionnaire.stories.js
+++ src/stories/questionnaire/questionnaire.stories.js
@@ -4,6 +4,7 @@ import Orchestrator from '../utils/orchestrator';
 import { titleDecorator } from 'utils/lib';
 import calcVar from './calc-var';
 import logement from './logement';
+import logementSequence from './logement-sequence';
 import simpsons from './simpsons';
 import arithmetic from './arithmetic';
 import { positioningOptions, featuresOptions } from '../utils/options';
@@ -90,6 +91,21 @@ paginated.addWithJSX('Logement', () => (
 	<Orchestrator
 		id="props"
 		source={logement}
+		missing={boolean('Missing', false)}
+		activeGoNextForMissing={boolean('Active go next for missing', false)}
+		features={select('Features', featuresOptions, ['VTL', 'MD'])}
+		positioning={select('Items positioning', positioningOptions, 'DEFAULT')}
+		disabled={boolean('Disabled', false)}
+		focused={boolean('Focused', false)}
+		management={boolean('Management', false)}
+		pagination
+	/>
+));
+
+paginated.addWithJSX('Logement - Sequence', () => (
+	<Orchestrator
+		id="props"
+		source={logementSequence}
 		missing={boolean('Missing', false)}
 		activeGoNextForMissing={boolean('Active go next for missing', false)}
 		features={select('Features', featuresOptions, ['VTL', 'MD'])}
