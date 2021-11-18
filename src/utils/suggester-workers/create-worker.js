7696f807933bc86cffc4ec9e3ef89a481fef1ef3
Laurent Caouissin <38245508+laurentC35@users.noreply.github.com>
2021-11-18 23:49:00 +0100

    Worker : fix web-worker for localhost

--- src/utils/suggester-workers/create-worker.js
+++ src/utils/suggester-workers/create-worker.js
@@ -1,25 +1,30 @@
 // Enable cross domain workers
-
 export const createWorker = (workerUrl) => {
 	var worker = null;
 	try {
-		var blob;
+		worker = new Worker(workerUrl);
+	} catch (e) {
 		try {
-			blob = new Blob([`importScripts('${workerUrl}');`], {
-				type: 'application/javascript',
-			});
-		} catch (e) {
-			var blobBuilder = new (window.BlobBuilder ||
-				window.WebKitBlobBuilder ||
-				window.MozBlobBuilder)();
-			blobBuilder.append(`importScripts('${workerUrl}');`);
-			blob = blobBuilder.getBlob('application/javascript');
+			var blob;
+			try {
+				blob = new Blob([`importScripts('${workerUrl}');`], {
+					type: 'application/javascript',
+				});
+			} catch (e) {
+				var blobBuilder = new (window.BlobBuilder ||
+					window.WebKitBlobBuilder ||
+					window.MozBlobBuilder)();
+				blobBuilder.append(`importScripts('${workerUrl}');`);
+				blob = blobBuilder.getBlob('application/javascript');
+			}
+			var url = window.URL || window.webkitURL;
+			var blobUrl = url.createObjectURL(blob);
+			worker = new Worker(blobUrl);
+		} catch (e1) {
+			console.error(
+				`Lunatic-worker : Failed to load web worker : ${workerUrl}`
+			);
 		}
-		var url = window.URL || window.webkitURL;
-		var blobUrl = url.createObjectURL(blob);
-		worker = new Worker(blobUrl);
-	} catch (e1) {
-		console.error(`Lunatic-worker : Failed to load web worker : ${workerUrl}`);
 	}
 	return worker;
 };
