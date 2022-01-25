11178420eca72d1ffd8389ec0439aecfce2d5937
NicoLaval <nicolas.laval@insee.fr>
2022-01-25 22:11:30 +0100

    Remove old tests

--- src/tests/components/sequence.spec.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { Sequence } from 'components';
-
-describe('sequence', () => {
-	it('renders without crashing', () => {
-		shallow(<Sequence id="module-id" label="Sequence" />);
-	});
-});
