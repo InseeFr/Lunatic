8f438c916d45c571b4f800bf8ab9caa66701862b
Renaud Genevois <KQHLZ2@ad.insee.intra>
2022-03-11 08:36:30 +0100

    suggester with mui

--- src/stories/suggester/suggester-material-ui.js
+++ src/stories/suggester/suggester-material-ui.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
+import IconButton from '@mui/material/IconButton';
 
 function SuggesterMaterialUI({
 	className,
@@ -9,9 +9,43 @@ function SuggesterMaterialUI({
 	optionRenderer,
 	labelRenderer,
 	onSelect,
+	onBlur,
+	onDelete,
+	onKeyDown,
+	onChange,
+	onClickOption,
 	value,
+	onFocus,
+	disabled,
+	id,
+	messageError,
+	search,
+	focused,
+	options,
+	expended,
+	selectedIndex,
+	displayLabel,
 }) {
-	return <>SuggesterMaterialUI</>;
+	const [muiOptions, setMUIOptions] = useState([]);
+
+	useEffect(
+		function () {
+			const op = options.map(function ({ label, id }) {
+				return { label, id };
+			});
+			setMUIOptions(op);
+		},
+		[options]
+	);
+
+	const onChangeEx = useCallback(
+		function (e) {
+			onChange(e.target.value);
+		},
+		[onChange]
+	);
+
+	return <TextField onChange={onChangeEx} />;
 }
 
 export default SuggesterMaterialUI;
