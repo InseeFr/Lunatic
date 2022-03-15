aae210fadc973fa45b8f7c3145ee3dc0d75bebd2
NicoLaval <nicolas.laval@insee.fr>
2022-03-15 13:20:13 +0100

    Remove outdated tests

--- src/tests/components/subsequence.spec.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { Subsequence } from 'components';
-
-describe('subsequence', () => {
-	it('renders without crashing', () => {
-		shallow(<Subsequence id="subsequence-id" label="Subsequence" />);
-	});
-});
