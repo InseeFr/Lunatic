137cf58053873882812b74966a8a9786a83b54d3
NicoLaval <nicolas.laval@insee.fr>
2022-06-02 10:45:49 +0200

    Update get-safety-expression.js

--- src/use-lunatic/commons/execute-expression/get-safety-expression.js
+++ src/use-lunatic/commons/execute-expression/get-safety-expression.js
@@ -2,6 +2,14 @@ function getSafetyExpression(expression) {
 	if (typeof expression === 'string') {
 		return { value: expression, type: 'VTL|MD' };
 	}
+	// TODO: fix into model and supress this block
+	if (
+		typeof expression === 'object' &&
+		'value' in expression &&
+		!('type' in expression)
+	) {
+		return { ...expression, type: 'VTL|MD' };
+	}
 
 	return expression;
 }
