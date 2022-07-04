ff72d4f4918de19ce9f04b4cb0e50fefbd04b7bf
NicoLaval <nicolas.laval@insee.fr>
2022-07-04 09:14:23 +0200

    Add remove btn to RosterForLoop

--- src/components/checkbox/checkbox-group/checkbox-group.js
+++ src/components/checkbox/checkbox-group/checkbox-group.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { CheckboxOption } from '../commons';
 import { Label } from '../../commons';
 import { useOnHandleChange } from '../../commons';
--- src/i18n/dictionary.js
+++ src/i18n/dictionary.js
@@ -1,5 +1,6 @@
 const dictionary = {
 	DEFAULT_BUTTON_ADD: { fr: 'Ajouter une ligne', en: 'Add row' },
+	DEFAULT_BUTTON_REMOVE: { fr: 'Supprimer une ligne', en: 'Remove row' },
 	DK: { fr: 'Ne sais pas', en: "Don't know" },
 	RF: { fr: 'Refus', en: 'Refused' },
 };
