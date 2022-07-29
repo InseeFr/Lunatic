ad86022ee9b39b6e544665deda08f85fca699188
NicoLaval <nicolas.laval@insee.fr>
2022-07-29 15:13:28 +0200

    Update FilterDescription component

--- src/components/filter-description/component.js
+++ src/components/filter-description/component.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
-const FilterDescription = ({ id, label, filterDescription }) =>
-	filterDescription ? (
-		<div
-			id={`filter-description-${id}`}
-			aria-label={`filter-description`}
-			className="filter-description-lunatic"
-		>
-			{label}
-		</div>
-	) : null;
+const FilterDescription = ({ id, label }) => (
+	<div
+		id={`filter-description-${id}`}
+		aria-label={`filter-description`}
+		className="filter-description-lunatic"
+	>
+		{label}
+	</div>
+);
 
 export default FilterDescription;
