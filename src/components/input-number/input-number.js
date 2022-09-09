42a5b606b62a09a0bb61e5c6df2366002868ece8
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2022-09-09 09:15:15 +0200

    Lunatic state : store null instead of NaN for input number

--- src/components/input-number/input-number.js
+++ src/components/input-number/input-number.js
@@ -18,7 +18,7 @@ function InputNumber({
 	const handleChange = useCallback(
 		function (e) {
 			const val = e.target.valueAsNumber;
-			onChange(val);
+			onChange(isNaN(val) ? null : val);
 		},
 		[onChange]
 	);
