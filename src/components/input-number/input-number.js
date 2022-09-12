7cc2ddc52906335bff05d1b2a9781aed35fefe79
NicoLaval <nicolas.laval@insee.fr>
2022-09-12 15:47:52 +0200

    Add constraint in InputNumber

--- src/components/input-number/input-number.js
+++ src/components/input-number/input-number.js
@@ -3,6 +3,12 @@ import classnames from 'classnames';
 import { createCustomizableLunaticField } from '../commons';
 import './input-number.scss';
 
+const regex = /[0-9]|e|E|,|\./;
+
+const handleKeyPress = function (e) {
+	if (!regex.test(e.key)) e.preventDefault();
+};
+
 function InputNumber({
 	value,
 	onChange,
@@ -16,17 +22,21 @@ function InputNumber({
 }) {
 	const handleChange = useCallback(
 		function (e) {
+			if (e.target.value === '') onChange(null);
 			const val = e.target.valueAsNumber;
-			onChange(isNaN(val) ? null : val);
+			if (isNaN(val)) e.preventDefault();
+			else onChange(val);
 		},
 		[onChange]
 	);
+
 	return (
 		<div className="lunatic-input-number-container">
 			<input
 				className={classnames('lunatic-input-number', { disabled, readOnly })}
 				type="number"
 				onChange={handleChange}
+				onKeyPress={handleKeyPress}
 				value={value ?? ''}
 				labelledby={labelId}
 				readOnly={readOnly}
