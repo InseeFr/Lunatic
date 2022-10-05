21e5f72731f5cf39669a9c11abe0f1d1f6c6e48c
Dylan Decrulle <dylan.decrulle@outlook.fr>
2022-10-05 16:58:06 +0200

    fix: modal always displayed in loop

--- src/use-lunatic/commons/index.js
+++ src/use-lunatic/commons/index.js
@@ -10,3 +10,4 @@ export { default as executeConditionFilter } from './execute-condition-filter';
 export { default as fillComponentExpressions } from './fill-components/fill-component-expressions';
 export { default as loadSuggester } from './load-suggesters';
 export { default as getCompatibleVTLExpression } from './get-compatible-vtl-expression';
+export { default as getErrorsWithoutEmptyValue } from './get-errors-without-empty-value';
