f50ac14fc43bd2e031eee302e7d5897775dedd12
Renaud Genevois <renaud.genevois@insee.fr>
2022-10-11 15:48:26 +0200

    value field null or undefined

---
+++ src/stories/suggester/suggester-workers.stories.js
@@ -0,0 +1,125 @@
+import React, { useState, useCallback } from 'react';
+import Orchestrator from '../utils/orchestrator';
+// import source from './source';
+import defaultArgTypes from '../utils/default-arg-types';
+import { clearDb, openOnCreateDb, insertEntity } from '../../utils/idb-tools';
+import { CONSTANTES } from '../../utils/store-tools';
+import append from '../../utils/suggester-workers/append-to-index';
+import searching from '../../utils/suggester-workers/searching';
+
+const stories = {
+	title: 'Components/Suggester-workers',
+	component: Orchestrator,
+	argTypes: defaultArgTypes,
+};
+
+export default stories;
+
+function fetchNaf() {
+	return fetch('/naf-rev2.json').then((r) => r.json());
+}
+
+function fetchFake() {
+	return fetch('/fake-nomenclature.json').then((r) => r.json());
+}
+
+const infoFake = {
+	name: 'fake',
+	fields: [
+		{
+			name: 'tags',
+			rules: ['[\\w]+'],
+			language: 'French',
+			min: 3,
+		},
+	],
+	queryParser: {
+		type: 'tokenized',
+		params: { language: 'French', pattern: '[\\w.]+' },
+	},
+	version: '1',
+};
+
+const infoNaf = {
+	name: 'naf-rev2',
+	fields: [
+		{
+			name: 'label',
+			rules: ['[\\w]+'],
+			language: 'French',
+			min: 3,
+		},
+		{ name: 'id' },
+	],
+	queryParser: {
+		type: 'tokenized',
+		params: { language: 'French', pattern: '[\\w.]+' },
+	},
+	version: '1',
+};
+
+async function loadOne(info, fetchIt) {
+	const { name } = info;
+	const rubriques = await fetchIt();
+	const db = await openOnCreateDb(name);
+	await clearDb(db, CONSTANTES.STORE_DATA_NAME);
+	await clearDb(db, CONSTANTES.STORE_INFO_NAME);
+	await append(info, '1', rubriques, console.log);
+	await insertEntity(db, CONSTANTES.STORE_INFO_NAME, info);
+}
+
+function Search({ storeInfo, version = '1', max = 30, defaultValue = '' }) {
+	const { name, order } = storeInfo;
+	const [value, setValue] = useState(defaultValue);
+	const onClick = useCallback(
+		function () {
+			async function doIt() {
+				const results = await searching(value, {
+					name,
+					version,
+					max,
+					order,
+				});
+				console.log(results);
+			}
+			if (value.length) {
+				doIt();
+			}
+		},
+		[value, name, version, max, order]
+	);
+	return (
+		<>
+			<input
+				type="search"
+				value={value}
+				onChange={(e) => setValue(e.target.value)}
+			/>
+			<input type="button" onClick={onClick} value="Search!" />
+		</>
+	);
+}
+
+const Template = (args) => (
+	<>
+		<ul>
+			<li>
+				<input
+					type="button"
+					value="load NAF"
+					onClick={() => loadOne(infoNaf, fetchNaf)}
+				/>
+				<Search storeInfo={infoNaf} defaultValue="culture du tabac" />
+			</li>
+			<li>
+				<input
+					type="button"
+					value="load FAKE"
+					onClick={() => loadOne(infoFake, fetchFake)}
+				/>
+				<Search storeInfo={infoFake} defaultValue="madame" />
+			</li>
+		</ul>
+	</>
+);
+export const Default = Template.bind({});
