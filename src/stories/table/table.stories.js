85639392a9d1b93be67180da559568959b0dae51
Dylan Decrulle <dylan.decrulle@outlook.fr>
2022-10-11 10:40:25 +0200

    add dynamic Table stories

--- src/stories/table/table.stories.js
+++ src/stories/table/table.stories.js
@@ -3,6 +3,7 @@ import Orchestrator from '../utils/orchestrator';
 import source from './source.json';
 import data from './data.json';
 import sourceRoster from './source-roster.json';
+import sourceDynamic from './table-dynamique.json';
 import dataRoster from './data-roster.json';
 import defaultArgTypes from '../utils/default-arg-types';
 import * as custom from '../custom-mui';
@@ -29,4 +30,11 @@ RosterMui.args = {
 	source: sourceRoster,
 	data: dataRoster,
 	custom,
+};
+
+export const DynamicTable = Template.bind({});
+
+DynamicTable.args = {
+	id: 'dynamic-table',
+	source: sourceDynamic,
 };
