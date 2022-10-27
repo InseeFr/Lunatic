abbed841de86aafdefcbff9fa18b4527037ba5ba
MickaelMenet <mickaelmenet@gmail.com>
2022-10-27 10:37:11 +0200

    Addition in the dictionary i18n for the buttons of the modal

--- src/i18n/dictionary.js
+++ src/i18n/dictionary.js
@@ -1,6 +1,8 @@
 const dictionary = {
 	DEFAULT_BUTTON_ADD: { fr: 'Ajouter une ligne', en: 'Add row' },
 	DEFAULT_BUTTON_REMOVE: { fr: 'Supprimer une ligne', en: 'Remove row' },
+	MODAL_IGNORE: { fr: 'Ignorer', en: 'Ignore' },
+	MODAL_CORRECT: { fr: 'Corriger', en: 'Correct' },
 	DK: { fr: 'Ne sais pas', en: "Don't know" },
 	RF: { fr: 'Refus', en: 'Refused' },
 };
