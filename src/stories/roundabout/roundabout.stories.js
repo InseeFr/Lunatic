2a133c2ddc5e984e8eb0db3800b13582e7f6ffc5
Renaud Genevois <renaud.genevois@insee.fr>
2023-01-11 10:44:41 +0100

    reste

--- src/stories/roundabout/roundabout.stories.js
+++ src/stories/roundabout/roundabout.stories.js
@@ -1,5 +1,3 @@
-import { useCallback } from 'react';
-import classnames from 'classnames';
 import Orchestrator from '../utils/orchestrator';
 import source from './source';
 import data from './data';
@@ -13,61 +11,7 @@ const stories = {
 
 export default stories;
 
-function getStatus(complete, partial) {
-	if (complete) {
-		return 'complete';
-	}
-	if (partial) {
-		return 'partial';
-	}
-	return 'unstarted';
-}
-
-function getLabel(complete, partial) {
-	if (complete) {
-		return 'Complété';
-	}
-	if (partial) {
-		return 'Modifier';
-	}
-	return 'Commencer';
-}
-
-function RoundaboutItButton({
-	complete,
-	partial,
-	iteration,
-	goToIteration,
-	locked,
-}) {
-	const status = getStatus(complete, partial);
-	const label = getLabel(complete, partial);
-
-	const onClick = useCallback(
-		function () {
-			goToIteration(iteration);
-		},
-		[iteration, goToIteration]
-	);
-
-	return (
-		<button
-			className={classnames('roundabout-it-button', status)}
-			disabled={status === 'complete' && locked}
-			onClick={onClick}
-		>
-			{label}
-		</button>
-	);
-}
-
-RoundaboutItButton.defaultProps = {
-	locked: true,
-};
-
-const custom = { RoundaboutItButton };
-
-const Template = (args) => <Orchestrator {...args} custom={custom} />;
+const Template = (args) => <Orchestrator {...args} />;
 export const Default = Template.bind({});
 
 Default.args = { id: 'roundabout', source, pagination: true, data };
