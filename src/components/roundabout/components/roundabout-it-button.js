376cb82aaea8eda4b4c6660fdeb0584d88aead99
Renaud Genevois <renaud.genevois@insee.fr>
2023-01-18 16:11:32 +0100

    unnecessary in roundabout

--- src/components/roundabout/components/roundabout-it-button.js
+++ src/components/roundabout/components/roundabout-it-button.js
@@ -13,7 +13,10 @@ function getStatus(complete, partial) {
 	return 'unstarted';
 }
 
-function getLabel(complete, partial) {
+function getLabel({ complete, partial, unnecessary }) {
+	if (unnecessary) {
+		return 'Non concerné';
+	}
 	if (complete) {
 		return 'Complété';
 	}
@@ -23,15 +26,24 @@ function getLabel(complete, partial) {
 	return 'Commencer';
 }
 
+function isDisabled({ status, locked, unnecessary }) {
+	if (unnecessary || (status === 'complete' && locked)) {
+		return true;
+	}
+
+	return false;
+}
+
 function RoundaboutItButton({
 	complete,
 	partial,
 	iteration,
+	unnecessary,
 	goToIteration,
 	locked,
 }) {
 	const status = getStatus(complete, partial);
-	const label = getLabel(complete, partial);
+	const label = getLabel({ complete, partial, unnecessary });
 
 	const onClick = useCallback(
 		function () {
@@ -43,7 +55,7 @@ function RoundaboutItButton({
 	return (
 		<Button
 			className={classnames('roundabout-it-button', status)}
-			disabled={status === 'complete' && locked}
+			disabled={isDisabled({ status, locked, unnecessary })}
 			onClick={onClick}
 		>
 			{label}
