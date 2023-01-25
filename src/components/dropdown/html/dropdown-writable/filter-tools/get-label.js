1ddbd0b3afdafe130d4ccb85f18f953aa2830358
Renaud Genevois <renaud.genevois@insee.fr>
2023-01-25 10:19:17 +0100

    comment on getLabel

--- src/components/dropdown/html/dropdown-writable/filter-tools/get-label.js
+++ src/components/dropdown/html/dropdown-writable/filter-tools/get-label.js
@@ -1,5 +1,12 @@
 import { isValidElement } from 'react';
 
+/**
+ * Les labels peuvent être des chaines des caractères ou des fragments JSX issus
+ * du composant de transformation Markdown (voir components/commons/components/md-label).
+ * A l'issu de cette fonction, celle demeurre la valeur textuelle du label.
+ * @param {*} label
+ * @returns une chaine de caractère
+ */
 function getLabel(label) {
 	if (isValidElement(label)) {
 		const { props } = label;
