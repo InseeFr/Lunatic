1d0b21892125277f0021c4ac11f4332324cb47f2
Laurent Caouissin <caouissin.laurent@gmail.com>
2023-02-28 14:24:48 +0100

    feat: add thousand separator for input where max is sperior to 1000

--- src/components/input-number/html/input-number-thousand.js
+++ src/components/input-number/html/input-number-thousand.js
@@ -1,14 +1,13 @@
 import classNames from 'classnames';
 import React, { useCallback } from 'react';
 import { NumericFormat } from 'react-number-format';
-import { inputNumberPropsI18N } from '../../i18n';
+import { inputNumberPropsI18N } from '../../../i18n';
 
 const InputNumberThousand = ({
 	id,
 	value,
 	onChange,
 	disabled,
-	readOnly,
 	labelId,
 	min,
 	max,
@@ -21,27 +20,34 @@ const InputNumberThousand = ({
 		},
 		[onChange]
 	);
-	return (
-		<NumericFormat
-			id={id}
-			className={classNames('lunatic-input-number', { disabled, readOnly })}
-			onValueChange={handleChange}
-			value={value ?? ''}
-			labelledby={labelId}
-			readOnly={readOnly}
-			disabled={disabled}
-			lang="en"
-			isAllowed={(values) => {
-				const { floatValue } = values;
+
+	const isAllowed = useCallback(
+		(values) => {
+			const { floatValue } = values;
+			if (Number.isInteger(min) && Number.isInteger(max))
 				return (
 					(floatValue >= min && floatValue <= max) || floatValue === undefined
 				);
-			}}
+			return true;
+		},
+		[max, min]
+	);
+	return (
+		<NumericFormat
+			id={id}
+			className={classNames({ disabled })}
+			onValueChange={handleChange}
+			value={value ?? ''}
+			labelledby={labelId}
+			disabled={disabled}
+			lang="en"
+			isAllowed={isAllowed}
 			allowedDecimalSeparators={inputNumberPropsI18N.allDecimalSeparators}
 			decimalSeparator={inputNumberPropsI18N.decimalSeparator}
 			decimalScale={decimals}
 			allowLeadingZeros
 			thousandSeparator={inputNumberPropsI18N.thousandSeparator}
+			inputMode={decimals ? 'decimal' : 'numeric'}
 		/>
 	);
 };
