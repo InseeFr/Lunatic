68dca0708ec5c5a1242dd14148159d5a88cf1fbd
Laurent Caouissin <38245508+laurentC35@users.noreply.github.com>
2023-03-01 12:29:52 +0100

    fix: follow sonar advices (#457)

--- src/components/commons/components/variable-status/variable-status.jsx
+++ src/components/commons/components/variable-status/variable-status.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import ReactTooltip from 'react-tooltip';
 import * as img from './img';
 import './variable-status.scss';
