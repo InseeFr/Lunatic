16ae4ff58a2aa11f9a7ab88a9c7a50ce6cf6851c
Mehdi <mehdi.haboucha@insee.fr>
2023-03-01 10:02:56 +0100

    test : test unitaire pour le composant common field-container.spec

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
 	plugins: [react()],
 	test: {
+		environment: 'jsdom',
 		include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+		setupFiles: './setup-tests.js',
 	},
 });
