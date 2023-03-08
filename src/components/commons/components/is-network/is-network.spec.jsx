e6892dbd9dfd7bbd63ef8d0ede6b3c6e43e1cfd0
Mehdi <mehdi.haboucha@insee.fr>
2023-03-08 11:19:22 +0100

    test : test unitaire pour le composant common isNetwork et le hook use-online-status

---
+++ src/components/commons/components/is-network/is-network.spec.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import IsNetwork from './is-network';
+import { expect, describe, it, vi, afterEach, beforeEach } from 'vitest';
+
+describe('IsNetwork', () => {
+	let originalNavigator;
+	beforeEach(() => {
+		originalNavigator = window.navigator;
+	});
+	afterEach(() => {
+		vi.restoreAllMocks();
+		window.navigator = originalNavigator;
+	});
+	it('renders the component', () => {
+		render(<IsNetwork />);
+		const component = screen.getByTitle(/network on/i);
+		expect(component).toBeInTheDocument();
+	});
+
+	it('displays "Network on" when online', () => {
+		render(<IsNetwork />);
+		const component = screen.getByTitle(/network on/i);
+		expect(component).toBeInTheDocument();
+	});
+
+	it('displays "Network off" when offline', () => {
+		vi.spyOn(window.navigator, 'onLine', 'get').mockReturnValue(false);
+		render(<IsNetwork />);
+		const component = screen.getByTitle(/network off/i);
+		expect(component).toBeInTheDocument();
+	});
+});
