e9c93ed9ee19fafc5b09183f8f3c4a785f9bad8c
Mehdi <mehdi.haboucha@insee.fr>
2023-03-12 13:14:02 +0100

    test : test unitaire pour le composant common fieldset

--- src/components/commons/components/description.spec.jsx
+++ src/components/commons/components/description.spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
-import Description, { DescritionPropsType } from './description';
+import Description from './description';
 
 describe('Description', () => {
 	const testArray = [
