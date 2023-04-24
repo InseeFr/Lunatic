d72e34ecf57a3048d484335133ed63235dec7653
Matthew Clark <matthew.clark@insee.fr>
2023-04-24 16:52:10 +0200

    regroup stories into one file

--- src/stories/component-set/component-set.stories.jsx
+++ src/stories/component-set/component-set.stories.jsx
@@ -1,6 +1,10 @@
 import Orchestrator from '../utils/orchestrator';
 import source from './source';
 import data from './data';
+import sourceLoop from './source1';
+import dataLoop from './data1';
+import sourceRoundabout from './source2';
+import dataRoundabout from './data2';
 import defaultArgTypes from '../utils/default-arg-types';
 
 const stories = {
@@ -15,3 +19,21 @@ const Template = (args) => <Orchestrator {...args} />;
 export const Default = Template.bind({});
 
 Default.args = { id: 'component-set', source, pagination: true, data };
+
+export const InRoundabout = Template.bind({});
+
+InRoundabout.args = {
+	id: 'component-set-roundabout',
+	source: sourceRoundabout,
+	pagination: true,
+	data: dataRoundabout,
+};
+
+export const InLoop = Template.bind({});
+
+InLoop.args = {
+	id: 'component-set-loop',
+	source: sourceLoop,
+	pagination: true,
+	data: dataLoop,
+};
