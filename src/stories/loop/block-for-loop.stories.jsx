837f5c21dbe26d5f7624f55577c1e5d747879df5
Bulot François <f_bulot@hotmail.com>
2023-06-01 11:05:01 +0200

    storybook : move and rename correctly roster-for-loop-with-header

--- src/stories/loop/block-for-loop.stories.jsx
+++ src/stories/loop/block-for-loop.stories.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Orchestrator from '../utils/orchestrator';
 import sourceBloc from './source-bloc.json';
-import sourceWithHeader from './source-with-header.json';
 import defaultArgTypes from '../utils/default-arg-types';
 
 const stories = {
@@ -16,6 +15,3 @@ const Template = (args) => <Orchestrator {...args} />;
 
 export const Default = Template.bind({});
 Default.args = { id: 'bloc-for-loop', source: sourceBloc };
-
-export const WithHeader = Template.bind({});
-WithHeader.args = { id: 'bloc-for-loop-headers', source: sourceWithHeader };
