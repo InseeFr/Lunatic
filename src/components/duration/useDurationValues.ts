1cbf2d53b4e158d925678bad2847546b4176ca29
MickaelMenet <mickaelmenet@gmail.com>
2023-06-06 15:14:16 +0200

    add comment on hook

--- src/components/duration/useDurationValues.ts
+++ src/components/duration/useDurationValues.ts
@@ -1,32 +1,43 @@
-
-export const useDurationValues = (value: string | null,format: Formats, handleChange: (v: string | null) => void) => {
-  const duration = parseISO8601Duration(value)
+export const useDurationValues = (
+  value: string | null,
+  format: Formats,
+  handleChange: (v: string | null) => void
+) => {
+  const duration = parseISO8601Duration(value);
+  // Gestionnaire de changement pour les champs individuels (heures, minutes, mois, années)
   const onChangeHandler = (key: keyof DurationValues) => (e: {target: {value: string}}) => {
-          handleChange(formatISO8601Duration(format,{...duration, [key]: e.target.value}))
-        }    
-  const propsHours = {value: duration.hours, onChange: onChangeHandler("hours"), min: "0", max:"23"}
-  const propsMinutes = {value: duration.minutes, onChange: onChangeHandler("minutes"), min: "0", max:"59"}
-  const propsMonths = {value: duration.months, onChange: onChangeHandler("months"), min: "0", max:"11"}
-  const propsYears = {value: duration.years, onChange: onChangeHandler("years"),min: "0"}
-  return {propsHours,propsMonths,propsYears,propsMinutes}
-}
-
-type DurationValues = {
-	years: string;
-	months: string;
-	hours: string;
-	minutes: string;
-}
-type Formats = "PTnHnM" | "PnYnM"
-const parseISO8601Duration = (duration: string | null | undefined): DurationValues => {
-  if(!duration)  return {
-    hours: '',
-    minutes: '',
-    years: '',
-    months: '',
+    // Mise à jour de la valeur de la durée avec la clé dynamique
+    handleChange(formatISO8601Duration(format, {...duration, [key]: e.target.value}));
   };
+  const propsHours = {value: duration.hours, onChange: onChangeHandler("hours"), min: "0", max:"23"};
+  const propsMinutes = {value: duration.minutes, onChange: onChangeHandler("minutes"), min: "0", max:"59"};
+  const propsMonths = {value: duration.months, onChange: onChangeHandler("months"), min: "0", max:"11"};
+  const propsYears = {value: duration.years, onChange: onChangeHandler("years"),min: "0"};
+  // Retourne les propriétés pour chaque champ de durée
+  return {propsHours, propsMonths, propsYears, propsMinutes};
+};
+type DurationValues = {
+  years: string;
+  months: string;
+  hours: string;
+  minutes: string;
+};
+type Formats = "PTnHnM" | "PnYnM";
+// Fonction pour parser une durée au format ISO8601 en objets individuels
+const parseISO8601Duration = (duration: string | null | undefined): DurationValues => {
+  // Si la durée est vide, retourne des valeurs vides pour chaque champ
+  if (!duration) {
+    return {
+      hours: '',
+      minutes: '',
+      years: '',
+      months: '',
+    };
+  }
+  // Expression régulière pour le format heures:minutes (PTnHnM)
   const regexHourMinute = /PT(\d+H)?(\d+M)?/i;
   const matchHourMinute = duration.match(regexHourMinute);
+  // Si la durée correspond au format heures:minutes (PTnHnM)
   if (matchHourMinute) {
     const [, hours, minutes] = matchHourMinute;
     return {
@@ -36,8 +47,10 @@ const parseISO8601Duration = (duration: string | null | undefined): DurationValu
       months: '',
     };
   }
+  // Expression régulière pour le format années:mois (PnYnM)
   const regexYearMonth = /P(\d+Y)?(\d+M)?/i;
   const matchYearMonth = duration.match(regexYearMonth);
+  // Si la durée correspond au format années:mois (PnYnM)
   if (matchYearMonth) {
     const [, years, months] = matchYearMonth;
     return {
@@ -47,6 +60,7 @@ const parseISO8601Duration = (duration: string | null | undefined): DurationValu
       minutes: '',
     };
   }
+  // Si la durée ne correspond à aucun des formats connus, retourne des valeurs vides pour chaque champ
   return {
     hours: '',
     minutes: '',
@@ -54,14 +68,19 @@ const parseISO8601Duration = (duration: string | null | undefined): DurationValu
     months: '',
   };
 };
-
-const formatISO8601Duration = (format: Formats,duration: DurationValues): string | null => {
-  if(!duration.hours && !duration.minutes && !duration.years && !duration.months) return null
+// Fonction pour formatter les objets individuels en durée au format souhaité
+const formatISO8601Duration = (format: Formats, duration: DurationValues): string | null => {
+  // Si tous les champs de durée sont vides, retourne null
+  if (!duration.hours && !duration.minutes && !duration.years && !duration.months) {
+    return null;
+  }
+  // Formatage de la durée en fonction du format spécifié
   if (format === 'PTnHnM') {
     return `PT${duration.hours ? `${duration.hours}H` : '0H'}${duration.minutes ? `${duration.minutes}M` : '0M'}`;
   }
   if (format === 'PnYnM') {
     return `P${duration.years ? `${duration.years}Y` : '0Y'}${duration.months ? `${duration.months}M` : '0M'}`;
   }
+  // Si le format spécifié n'est pas valide, retourne null
   return null;
 };
