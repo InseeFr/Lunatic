580a9f3876a1ed1349d05ccb23a15e85eab18aca
MickaelMenet <mickaelmenet@gmail.com>
2023-06-13 10:56:34 +0200

    add wrapper & type

--- src/components/duration/duration.tsx
+++ src/components/duration/duration.tsx
@@ -1,22 +1,25 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import './duration.scss';
 import { useDurationValues } from './useDurationValues';
 import { useOnHandleChange } from '../commons';
+import { LunaticComponentProps } from '../type';
+import LunaticComponent from '../commons/components/lunatic-component-without-label';
 
-type Formats = 'PTnHnM' | 'PnYnM';
-const Duration = ({
-	value,
-	label,
-	format,
-	handleChange,
-	response,
-}: {
-	value: string;
-	label: ReactNode;
-	format: Formats;
-	handleChange: any;
-	response: any;
-}) => {
+function Duration(props: LunaticComponentProps<'Duration'>) {
+	const {
+		value,
+		label,
+		format,
+		handleChange,
+		response,
+		id,
+		preferences,
+		declarations,
+		missing,
+		missingResponse,
+		management,
+		description,
+	} = props;
 	const onChange = useOnHandleChange({
 		handleChange,
 		response,
@@ -26,66 +29,78 @@ const Duration = ({
 	const { propsHours, propsMinutes, propsYears, propsMonths } =
 		useDurationValues(value, format, onChange);
 	return (
-		<div className="container">
-			{label}
-			<div></div>
-			{format === 'PTnHnM' && (
-				<div className="field-group">
-					<div className="input-field-group">
-						<label htmlFor="hoursInput" className="input-label">
-							Heures:
-						</label>
-						<input
-							id="hoursInput"
-							type="number"
-							{...propsHours}
-							className="input-field"
-						/>
+		<LunaticComponent
+			id={id}
+			preferences={preferences}
+			declarations={declarations}
+			value={value}
+			missing={missing}
+			missingResponse={missingResponse}
+			management={management}
+			description={description}
+			handleChange={handleChange}
+		>
+			<div className="container">
+				{label}
+				<div></div>
+				{format === 'PTnHnM' && (
+					<div className="field-group">
+						<div className="input-field-group">
+							<label htmlFor="hoursInput" className="input-label">
+								Heures:
+							</label>
+							<input
+								id="hoursInput"
+								type="number"
+								{...propsHours}
+								className="input-field"
+							/>
+						</div>
+						<div className="input-field-group">
+							<label htmlFor="minutesInput" className="input-label">
+								Minutes:
+							</label>
+							<input
+								id="minutesInput"
+								type="number"
+								{...propsMinutes}
+								min="0"
+								className="input-field"
+							/>
+						</div>
 					</div>
-					<div className="input-field-group">
-						<label htmlFor="minutesInput" className="input-label">
-							Minutes:
-						</label>
-						<input
-							id="minutesInput"
-							type="number"
-							{...propsMinutes}
-							min="0"
-							className="input-field"
-						/>
+				)}
+				{format === 'PnYnM' && (
+					<div className="field-group">
+						<div className="input-field-group">
+							<label htmlFor="yearsInput" className="input-label">
+								Années:
+							</label>
+							<input
+								id="yearsInput"
+								type="number"
+								{...propsYears}
+								min="0"
+								className="input-field"
+							/>
+						</div>
+						<div className="input-field-group">
+							<label htmlFor="monthsInput" className="input-label">
+								Mois:
+							</label>
+							<input
+								id="monthsInput"
+								type="number"
+								{...propsMonths}
+								min="0"
+								className="input-field"
+							/>
+						</div>
 					</div>
-				</div>
-			)}
-			{format === 'PnYnM' && (
-				<div className="field-group">
-					<div className="input-field-group">
-						<label htmlFor="yearsInput" className="input-label">
-							Années:
-						</label>
-						<input
-							id="yearsInput"
-							type="number"
-							{...propsYears}
-							min="0"
-							className="input-field"
-						/>
-					</div>
-					<div className="input-field-group">
-						<label htmlFor="monthsInput" className="input-label">
-							Mois:
-						</label>
-						<input
-							id="monthsInput"
-							type="number"
-							{...propsMonths}
-							min="0"
-							className="input-field"
-						/>
-					</div>
-				</div>
-			)}
-		</div>
+				)}
+			</div>
+		</LunaticComponent>
 	);
-};
+}
 
 export default Duration;
