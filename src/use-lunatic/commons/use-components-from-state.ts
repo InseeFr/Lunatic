176d9862603457251bc87db19fdf7a8c6c96f3cb
Jonathan <395137+Grafikart@users.noreply.github.com>
2023-06-21 10:10:23 +0200

    fix: avoid rerender when state changed
    MR #572
    Fix #559
    Fix #563
    Helps #571

--- src/use-lunatic/commons/use-components-from-state.ts
+++ src/use-lunatic/commons/use-components-from-state.ts
@@ -1,28 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useMemo } from 'react';
 import getComponentsFromState from './get-components-from-state';
 import fillComponents from './fill-components';
 import { LunaticState } from '../type';
-import { LunaticComponentProps } from './fill-components/fill-components';
 
 function useComponentsFromState(state: LunaticState) {
-	const [componentsFilled, setComponentsFilled] = useState(
-		[] as LunaticComponentProps[]
-	);
-
-	useEffect(
+	return useMemo(
 		function () {
 			const { pager, pages, isInLoop } = state;
 			const components = getComponentsFromState({ pager, pages, isInLoop });
 			const filled = fillComponents(components, state);
-			const filtered = filled.filter(({ conditionFilter }) => {
+			return filled.filter(({ conditionFilter }) => {
 				return conditionFilter !== undefined ? conditionFilter : true;
 			});
-			setComponentsFilled(filtered);
 		},
 		[state]
 	);
-
-	return componentsFilled;
 }
 
 export default useComponentsFromState;
