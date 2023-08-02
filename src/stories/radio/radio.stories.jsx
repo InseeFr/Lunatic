dc705b2f72e6907810ccf3dfb90e3b38d519fc9b
Renaud Genevois <renaud.genevois@insee.fr>
2023-08-02 09:18:49 +0200

    add story for bug on radio for boolean option and false value

--- src/stories/radio/radio.stories.jsx
+++ src/stories/radio/radio.stories.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Orchestrator from '../utils/orchestrator';
 import source from './source';
+import sourceBoolean from './source-boolean';
 import defaultArgTypes from '../utils/default-arg-types';
+import data from './data';
 
 const stories = {
 	title: 'Components/Radio',
@@ -22,3 +24,7 @@ const Template = (args) => <Orchestrator {...args} />;
 export const Default = Template.bind({});
 
 Default.args = { id: 'radio', source, shortcut: true };
+
+export const BooleanRadio = Template.bind({});
+
+BooleanRadio.args = { id: 'radio', source: sourceBoolean, data };
