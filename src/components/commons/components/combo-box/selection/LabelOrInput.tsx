d7101dc4cb563edbb7ec374e2a5f44ed0fc28256
Renaud Genevois <renaud.genevois@insee.fr>
2023-08-08 14:51:33 +0200

    add comment on LabelOrInput

--- src/components/commons/components/combo-box/selection/LabelOrInput.tsx
+++ src/components/commons/components/combo-box/selection/LabelOrInput.tsx
@@ -4,6 +4,16 @@ import Input, { InputProps } from './input';
 type LabelOrInputTypeProps = LabelSelectionProps &
 	InputProps & { editable?: boolean; expanded?: boolean };
 
+/**
+ * Affiche l'input ou le libellé de la selection selon l'état du suggester.
+ * Lorsque le composant n'est pas focus, une représentation de la recherche active
+ * est présenté plutôt que le champ input. Ce dernier est liè à la recherche de l'utilisateur
+ * (les lettres saisies), non directement à l'option choisi parmi les suggestions.
+ * Utiliser directement le champ input pour représenter la selection entre en conflit avec les inter-action de recherche
+ * de l'utilisateur.
+ * @param props
+ * @returns
+ */
 export function LabelOrInput(props: LabelOrInputTypeProps) {
 	const { editable, expanded } = props;
 	const displayLabel = !editable || !expanded;
