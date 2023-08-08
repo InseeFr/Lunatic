e92ab70b65921f521db07c8a642a89ed0472ce3e
Matthew Clark <matthew.clark@insee.fr>
2023-08-08 17:09:34 +0200

    prettier formatting

--- src/components/modal/html/modal.tsx
+++ src/components/modal/html/modal.tsx
@@ -1,86 +1,73 @@
-import { useCallback } from "react";
-import { createCustomizableLunaticField } from "../../commons";
-import { LunaticComponentProps } from "../../type";
-import ReactDOM from "react-dom";
-import "./modal.scss"
-import Button from "../../button";
+import { useCallback } from 'react';
+import { createCustomizableLunaticField } from '../../commons';
+import { LunaticComponentProps } from '../../type';
+import ReactDOM from 'react-dom';
+import './modal.scss';
+import Button from '../../button';
 
+function Modal(
+	props: Pick<
+		LunaticComponentProps<'Modal'>,
+		| 'id'
+		| 'label'
+		| 'description'
+		| 'goToPage'
+		| 'page'
+		| 'goNextPage'
+		| 'goPreviousPage'
+	>
+) {
+	const { id, label, description, goToPage, page, goNextPage, goPreviousPage } =
+		props;
 
-function Modal(props: Pick<LunaticComponentProps<'Modal'>, 
-  'id' |
-  'label' |
-  'description' |
-  'goToPage' | 
-  'page' | 
-  'goNextPage' | 
-  'goPreviousPage'
->) {
-	const {
-		id,
-		label,
-		description,
-    goToPage, 
-    page, 
-    goNextPage, 
-    goPreviousPage
-	} = props;
-
-  const handleNextClick = useCallback(
+	const handleNextClick = useCallback(
 		function () {
-      // If goNextPage is explicitly passed down by the orchestrator, it is used. 
-      if (goNextPage) {
-        goNextPage();
-      } else {
-      // Else we go to page n+1 
-        const nextPage = parseInt(page) + 1 
-        goToPage({
-          page: `${nextPage}`
-        });
-      }
+			// If goNextPage is explicitly passed down by the orchestrator, it is used.
+			if (goNextPage) {
+				goNextPage();
+			} else {
+				// Else we go to page n+1
+				const nextPage = parseInt(page) + 1;
+				goToPage({
+					page: `${nextPage}`,
+				});
+			}
 		},
 		[goToPage, page, goNextPage]
 	);
-  const handlePreviousClick = useCallback(
+	const handlePreviousClick = useCallback(
 		function () {
-      // If goPreviousPage is explicitly passed down by the orchestrator, it is used. 
-      if (goPreviousPage) {
-        goPreviousPage();
-      } else { 
-      // Else we go to page n-1 
-        const previousPage = parseInt(page) - 1 
-        goToPage({
-          page: `${previousPage}`
-        });
-      }
+			// If goPreviousPage is explicitly passed down by the orchestrator, it is used.
+			if (goPreviousPage) {
+				goPreviousPage();
+			} else {
+				// Else we go to page n-1
+				const previousPage = parseInt(page) - 1;
+				goToPage({
+					page: `${previousPage}`,
+				});
+			}
 		},
 		[goToPage, page, goPreviousPage]
 	);
 
-  return ReactDOM.createPortal(
+	return ReactDOM.createPortal(
 		<div className="lunatic-modal">
 			<div className="modal-content">
-        <div id={id} className="lunatic-modal-container">
-          <div className="close-button">
-            <a onClick={handlePreviousClick}>fermer x</a>
-          </div>
-          <div className="modal-message">
-            <span>{label}</span>
-            <span>{description}</span>
-          </div>
-          <Button
-            onClick={handlePreviousClick}
-          >
-            Annuler
-          </Button>
-          <Button
-            onClick={handleNextClick}
-          >
-            Je confirme
-          </Button>
-        </div>
-      </div>
+				<div id={id} className="lunatic-modal-container">
+					<div className="close-button">
+						<a onClick={handlePreviousClick}>fermer x</a>
+					</div>
+					<div className="modal-message">
+						<span>{label}</span>
+						<span>{description}</span>
+					</div>
+					<Button onClick={handlePreviousClick}>Annuler</Button>
+					<Button onClick={handleNextClick}>Je confirme</Button>
+				</div>
+			</div>
 		</div>,
 		document.body
-	); 
+	);
 }
 export default createCustomizableLunaticField(Modal, 'Modal');
--- src/components/modal/lunatic-modal.tsx
+++ src/components/modal/lunatic-modal.tsx
@@ -5,33 +5,26 @@ import { LunaticComponentProps } from '../type';
 function empty() {}
 
 function LunaticModal(props: LunaticComponentProps<'Modal'>) {
-	const {
-		id,
-		label,
-		description,
-    goToPage,
-    page, 
-    goNextPage, 
-    goPreviousPage
-	} = props;
-  console.log(props)
+	const { id, label, description, goToPage, page, goNextPage, goPreviousPage } =
+		props;
+	console.log(props);
 
 	return (
 		<LunaticComponent
-    id={id}
-    label={label}
-    handleChange={empty}
-    value={undefined}
-    description={description}
+			id={id}
+			label={label}
+			handleChange={empty}
+			value={undefined}
+			description={description}
 		>
 			<Modal
 				id={id}
 				label={label}
-        page={page}
+				page={page}
 				description={description}
-        goToPage={goToPage}
-        goNextPage={goNextPage}
-        goPreviousPage={goPreviousPage}
+				goToPage={goToPage}
+				goNextPage={goNextPage}
+				goPreviousPage={goPreviousPage}
 			/>
 		</LunaticComponent>
 	);
