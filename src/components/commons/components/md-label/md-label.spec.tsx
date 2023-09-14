aa3da9c470472f881964dccb37056d620fa1af9c
Jonathan <contact@grafikart.fr>
2023-09-14 09:34:09 +0200

    test: emoji test

---
+++ src/components/commons/components/md-label/md-label.spec.tsx
@@ -0,0 +1,18 @@
+import { describe, expect, it } from 'vitest';
+import { render } from '@testing-library/react';
+import MdLabel from './md-label';
+
+describe('md-label', () => {
+	it('should handle emoticon', () => {
+		const obj = render(<MdLabel expression="**Demo** of a :dog:" />);
+		return expect(obj.container).toMatchInlineSnapshot(`
+			<p>
+			  <strong>
+			    Demo
+			  </strong>
+			   of a 
+			  🐶
+			</p>
+		`);
+	});
+});
