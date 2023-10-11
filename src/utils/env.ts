a37cc7d63535562269f4d56e896acbf7cc12002f
Jonathan <395137+Grafikart@users.noreply.github.com>
2023-10-11 09:44:03 +0200

    Remove meta env (#713)
    Co-authored-by: Quentin Ruhier <quentin.ruhier@insee.fr>

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,4 +1,4 @@
 export function isTestEnv(): boolean {
-	const meta = import.meta as any;
-	return meta && meta?.env ? (import.meta as any).env.MODE === 'test' : false;
+	// @ts-ignore
+	return import.meta.env.MODE === 'test';
 }
