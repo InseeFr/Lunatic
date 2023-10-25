5098bce12bb9a515fa0d54429e20fa1d5e025a34
Laurent Caouissin <caouissin.laurent@gmail.com>
2023-10-25 18:34:40 +0200

    fix: searching worker

--- src/components/suggester/searching/create-searching.ts
+++ src/components/suggester/searching/create-searching.ts
@@ -5,21 +5,18 @@ import {
 	getWorkerPath,
 } from '../../../utils/suggester-workers/worker-path';
 
-let WORKER: Worker | null;
-
 export function isWorkerCompatible(): boolean {
 	return !!window.Worker;
 }
 
 function getWorker(resolve: Function): Worker {
-	if (!WORKER) {
-		WORKER = createWorker(getWorkerPath(WorkerEnum.SEARCH));
-		WORKER!.addEventListener('message', function (e) {
-			const { data } = e;
-			resolve(data);
-			WORKER?.terminate();
-		});
-	}
+	const WORKER = createWorker(getWorkerPath(WorkerEnum.SEARCH));
+	WORKER!.addEventListener('message', function (e) {
+		const { data } = e;
+		resolve(data);
+		WORKER?.terminate();
+	});
+
 	return WORKER!;
 }
 
