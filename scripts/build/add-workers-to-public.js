c56593e1480b52dc055c13bc997bbadce84a5495
Laurent Caouissin <caouissin.laurent@gmail.com>
2023-10-27 14:44:23 +0200

    build: skip postinstall for lunatic

--- scripts/build/add-workers-to-public.js
+++ scripts/build/add-workers-to-public.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 const fs = require('fs');
+const path = require('path');
 const { currentPublicFolder, workersPath } = require('./workers-paths');
+
+const currentPackageJson = require(path.resolve(
+	`${process.cwd()}/package.json`
+));
+const lunaticPackageJson = require('../../package.json');
 
 /**
  * Copy worker only if not exists
@@ -41,4 +47,14 @@ const copyWorkerToPublicFolder = () => {
 	}
 };
 
-copyWorkerToPublicFolder();
+const isLunaticPostInstall = () => {
+	const lunaticName = lunaticPackageJson.name;
+	const currentName = currentPackageJson.name;
+
+	const currentNpmLiveCycle = process.env.npm_lifecycle_event;
+	return lunaticName === currentName && currentNpmLiveCycle === 'postinstall';
+};
+
+if (isLunaticPostInstall()) {
+	console.log('PostInstall skiped !');
+} else copyWorkerToPublicFolder();
