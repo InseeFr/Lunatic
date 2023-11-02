65eead91c55b4ea66b124825be29daf9ddcb454a
Jonathan <contact@grafikart.fr>
2023-11-02 20:00:53 +0100

    chore: remove unused icons

--- src/components/commons/icons/index.ts
+++ src/components/commons/icons/index.ts
@@ -4,7 +4,3 @@ export { OpenedIcon } from './opened-icon';
 export { LoadIcon } from './load-icon';
 export { NetworkIcon } from './network-icon';
 export { OnDragIcon } from './on-drag-icon';
-export { RadioCheckedIcon as RadioChecked } from './radio-checked-icon';
-export { RadioUncheckedIcon as RadioUnchecked } from './radio-unchecked-icon';
-export { CheckboxCheckedIcon as CheckboxChecked } from './checkbox-checked-icon';
-export { CheckboxUncheckedIcon as CheckboxUnchecked } from './checkbox-unchecked-icon';
