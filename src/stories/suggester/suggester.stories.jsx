3dffe1761db55214b749db2d05725184b9683ff4
Laurent Caouissin <38245508+laurentC35@users.noreply.github.com>
2023-11-03 12:50:37 +0100

    fix: #754 (#755)

--- src/stories/suggester/suggester.stories.jsx
+++ src/stories/suggester/suggester.stories.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import Orchestrator from '../utils/orchestrator';
-import source from './source';
-import simple from './simple';
-import sourceComponentSet from './source-component-set';
 import defaultArgTypes from '../utils/default-arg-types';
+import Orchestrator from '../utils/orchestrator';
+import { getReferentiel } from '../utils/referentiel';
+import simple from './simple';
+import source from './source';
+import sourceComponentSet from './source-component-set';
 
 const stories = {
 	title: 'Components/Suggester',
@@ -15,22 +16,6 @@ export default stories;
 
 const Template = (args) => <Orchestrator {...args} />;
 export const Default = Template.bind({});
-
-async function getReferentiel(name) {
-	switch (name) {
-		case 'naf-rev2-stop':
-		case 'naf-rev2':
-			return fetch(
-				'https://inseefr.github.io/Lunatic/storybook/naf-rev2.json'
-			).then((r) => r.json());
-		case 'cog-communes':
-			return fetch(
-				'https://inseefr.github.io/Lunatic/storybook/communes-2019.json'
-			).then((r) => r.json());
-		default:
-			throw new Error(`Unkonw référentiel ${name}`);
-	}
-}
 
 Default.args = {
 	id: 'suggester',
