c9b982c8cdb049408e2a50dc475a131ddfb52b80
Renaud Genevois <renaud.genevois@insee.fr>
2023-11-22 15:50:39 +0100

    remove unsused changes

--- src/utils/suggester-workers/searching/searching.worker.js
+++ src/utils/suggester-workers/searching/searching.worker.js
@@ -3,14 +3,9 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import searching from './searching';
 
-let LAST_SEARCH = undefined;
-
 self.onmessage = function (e) {
 	const { search, name, version, meloto } = e.data;
-	LAST_SEARCH = search;
 	searching(search, { name, version, meloto }).then(function (result) {
-		if (search === LAST_SEARCH) {
-			self.postMessage(result);
-		}
+		self.postMessage(result);
 	});
 };
