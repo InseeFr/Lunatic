94980fd4005540d6a90de95ae2d5e713e64c6bf4
Laurent Caouissin <caouissin.laurent@gmail.com>
2023-11-23 16:20:51 +0100

    test: add new story for table

--- src/stories/table/table.stories.jsx
+++ src/stories/table/table.stories.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import Orchestrator from '../utils/orchestrator';
-import source from './source.json';
-import data from './data.json';
-import sourceRoster from './source-roster.json';
-import sourceDynamic from './table-dynamique.json';
-import dataRoster from './data-roster.json';
-import defaultArgTypes from '../utils/default-arg-types';
 import * as custom from '../custom-mui';
+import defaultArgTypes from '../utils/default-arg-types';
+import Orchestrator from '../utils/orchestrator';
+import dataRoster from './data-roster.json';
+import data from './data.json';
+import sourceColspan from './source-colspan.json';
+import sourceRoster from './source-roster.json';
+import source from './source.json';
+import sourceDynamic from './table-dynamique.json';
 
 const stories = {
 	title: 'Components/Table',
@@ -37,4 +38,10 @@ export const DynamicTable = Template.bind({});
 DynamicTable.args = {
 	id: 'dynamic-table',
 	source: sourceDynamic,
+};
+
+export const ColspanTable = Template.bind({});
+ColspanTable.args = {
+	id: 'colspan-table',
+	source: sourceColspan,
 };
