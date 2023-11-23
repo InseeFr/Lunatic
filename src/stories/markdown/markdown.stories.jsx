42ccb510dd4702d079b50725fc9c6e2152d89409
Jonathan <contact@grafikart.fr>
2023-11-23 10:46:44 +0100

    fix: merge error

--- src/stories/markdown/markdown.stories.jsx
+++ src/stories/markdown/markdown.stories.jsx
@@ -1,4 +1,3 @@
-<<<<<<<< HEAD:src/stories/markdown/markdown.stories.jsx
 import React from 'react';
 import Orchestrator from '../utils/orchestrator';
 import defaultArgTypes from '../utils/default-arg-types';
@@ -6,14 +5,6 @@ import source from './source.json';
 
 const stories = {
 	title: 'Components/Markdown',
-========
-import Orchestrator from '../utils/orchestrator';
-import source from './source';
-import defaultArgTypes from '../utils/default-arg-types';
-
-const stories = {
-	title: 'Components/QuestionContext',
->>>>>>>> 065af906 (ref: cleaned stories):src/stories/question-context/question-context.stories.jsx
 	component: Orchestrator,
 	argTypes: defaultArgTypes,
 };
@@ -23,11 +14,7 @@ export default stories;
 const Template = (args) => <Orchestrator {...args} />;
 export const Default = Template.bind({});
 
-<<<<<<<< HEAD:src/stories/markdown/markdown.stories.jsx
 Default.args = {
 	id: 'markdown',
 	source: source,
 };
-========
-Default.args = { id: 'questionExplication', source };
->>>>>>>> 065af906 (ref: cleaned stories):src/stories/question-context/question-context.stories.jsx
