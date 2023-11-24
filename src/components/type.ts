95761357f7ae1f27d5631e041492b008f1e5c54d
Jonathan <contact@grafikart.fr>
2023-11-24 11:37:27 +0100

    feat: colspan / rowspan handling
    Fix #802

--- src/components/type.ts
+++ src/components/type.ts
@@ -113,7 +113,10 @@ type ComponentPropsByType = {
 			rowspan?: number;
 			colspan?: number;
 		}>;
-		body: FilledLunaticComponentProps[][];
+		body: (FilledLunaticComponentProps & {
+			colspan?: number;
+			rowspan?: number;
+		})[][];
 		executeExpression: LunaticState['executeExpression'];
 		iteration: LunaticState['pager']['iteration'];
 	};
