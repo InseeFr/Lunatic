5a10d38e30899505d1b019d13298d17784c7eae4
jonathan <contact@grafikart.fr>
2023-12-07 16:40:24 +0100

    fix: skip indexedb for empty search

--- src/utils/suggester-workers/searching/searching.js
+++ src/utils/suggester-workers/searching/searching.js
@@ -54,18 +54,26 @@ async function searching(search, { name, version = '1', meloto = true }) {
 	try {
 		if (isValideSearch(search)) {
 			const db = await getDb(name, version);
-			const info = await getEntity(db, CONSTANTES.STORE_INFO_NAME, name);
+			const info = await getEntity(db, CONSTANTES.STORE_INFO_NAME, name, 'readonly');
 			const { queryParser, max, order } = info;
 			const parser = await resolveQueryParser(queryParser);
+			const tokens = parser(search);
+
+			// Do not start a transaction if we have nothing to search
+			if (tokens.length === 0) {
+				return {
+					results: [],
+					search,
+					tokens
+				}
+			}
 			const transaction = db.transaction(
 				CONSTANTES.STORE_DATA_NAME,
 				'readonly'
 			);
 			const store = transaction.objectStore(CONSTANTES.STORE_DATA_NAME);
 			const index = store.index(CONSTANTES.STORE_INDEX_NAME);
-			const tokens = parser(search);
 			const documents = await searchTokens(tokens, index);
-
 			return {
 				results: prepare(
 					getOrderingFunction(order)(
