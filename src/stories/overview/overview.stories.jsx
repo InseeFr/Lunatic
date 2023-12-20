aa05d2b255bcb164b7681de2f032f5f378d00e8f
Laurent Caouissin <caouissin.laurent@gmail.com>
2023-12-20 17:30:09 +0100

    test: add new story for overview

--- src/stories/overview/overview.stories.jsx
+++ src/stories/overview/overview.stories.jsx
@@ -1,8 +1,9 @@
-import Orchestrator from '../utils/orchestrator';
 import React from 'react';
-import data from './data';
 import defaultArgTypes from '../utils/default-arg-types';
+import Orchestrator from '../utils/orchestrator';
+import data from './data';
 import source from './sourceWithHierarchy';
+import sourceLoop from './withLoop';
 
 const stories = {
 	title: 'Behaviour/Overview',
@@ -14,5 +15,12 @@ export default stories;
 
 const Template = (args) => <Orchestrator {...args} />;
 export const Default = Template.bind({});
+export const WithLoop = Template.bind({});
 
 Default.args = { id: 'overview', source, showOverview: true, data };
+WithLoop.args = {
+	id: 'overview-loop',
+	source: sourceLoop,
+	showOverview: true,
+	data,
+};
