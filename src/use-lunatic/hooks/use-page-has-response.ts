2855e5b3674b5cd903078ef47fe99e5d1ff1a7e2
Laurent Caouissin <caouissin.laurent@gmail.com>
2023-12-20 16:24:37 +0100

    Fix #840

--- src/use-lunatic/hooks/use-page-has-response.ts
+++ src/use-lunatic/hooks/use-page-has-response.ts
@@ -78,6 +78,13 @@ function isSubComponentsEmpty(
 	executeExpression: LunaticState['executeExpression']
 ): boolean {
 	for (const component of components) {
+		if ('responses' in component) {
+			for (const response of component.responses) {
+				if (!isEmpty(executeExpression(response.response.name))) {
+					return false;
+				}
+			}
+		}
 		if (
 			'response' in component &&
 			!isEmpty(executeExpression(component.response.name))
