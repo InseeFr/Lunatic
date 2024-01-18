524926e31633c944a1859d174195dac700081c92
ddecrulle <ddecrulle@users.noreply.github.com>
2024-01-18 12:40:51 +0000

    deploy: 8cd587451bf078383a26277f83c531f8d2e46377

---
+++ storybook-2.7/assets/default-arg-types-a1f723ba.js
@@ -0,0 +1,2 @@
+const a=["source","id","pagination","data","management","activeControls","features","initialPage","getStoreInfo","missing","shortcut","filterDescription"],i=a.reduce((e,t)=>({...e,[t]:{table:{disable:!0}}}),{});export{i as d};
+//# sourceMappingURL=default-arg-types-a1f723ba.js.map
