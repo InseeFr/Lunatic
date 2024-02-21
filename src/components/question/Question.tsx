3526b308bb21cae149f6327f8fd8f858ec4ca973
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2024-02-21 16:45:21 +0100

    format

--- src/components/question/Question.tsx
+++ src/components/question/Question.tsx
@@ -7,32 +7,32 @@ import type { LunaticComponentProps } from '../type';
  * Question that must represent the Don dilman's definition of it.
  */
 export const Question = createCustomizableLunaticField(
-  (props: LunaticComponentProps<'Question'>) => {
-    const { id, description, declarations, label, components } = props;
-    return (
-      <>
-        <DeclarationsBeforeText declarations={declarations} id={id} />
-        <fieldset>
-          <legend id={`question-legend-${id}`}>
-            <h3>{label}</h3>
-            <p
-              className="description-lunatic"
-              id={`question-description-${id}`}
-            >
-              {description}
-            </p>
-          </legend>
+	(props: LunaticComponentProps<'Question'>) => {
+		const { id, description, declarations, label, components } = props;
+		return (
+			<>
+				<DeclarationsBeforeText declarations={declarations} id={id} />
+				<fieldset>
+					<legend id={`question-legend-${id}`}>
+						<h3>{label}</h3>
+						<p
+							className="description-lunatic"
+							id={`question-description-${id}`}
+						>
+							{description}
+						</p>
+					</legend>
 
-          <DeclarationsAfterText declarations={declarations} id={id} />
-          <LunaticComponents
-            components={components}
-            wrapper={({ children }) => (
-              <div className="question-components">{children}</div>
-            )}
-          />
-        </fieldset>
-      </>
-    );
-  },
-  'Question'
+					<DeclarationsAfterText declarations={declarations} id={id} />
+					<LunaticComponents
+						components={components}
+						wrapper={({ children }) => (
+							<div className="question-components">{children}</div>
+						)}
+					/>
+				</fieldset>
+			</>
+		);
+	},
+	'Question'
 );
