9de84e09b2a7736d3195cbeed20fcbed100690f3
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2024-02-26 14:50:36 +0100

    remove comment in story

--- src/stories/question/question.stories.jsx
+++ src/stories/question/question.stories.jsx
@@ -2,7 +2,6 @@ import Orchestrator from '../utils/orchestrator';
 import source from './source.json';
 
 import defaultArgTypes from '../utils/default-arg-types';
-//import type { Meta, StoryObj } from '@storybook/react';
 
 const meta = {
 	title: 'Components/Question',
@@ -11,11 +10,7 @@ const meta = {
 		...defaultArgTypes,
 	},
 };
-// } satisfies Meta<typeof Orchestrator>;
 
 export default meta;
 
-//type Story = StoryObj<typeof Orchestrator>
-
 export const Default = { args: { id: 'sequence-simple', source } };
-// satisfies Story
