6019dec99693446bb861aa9129d72502a1d3b0b3
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2024-02-27 15:34:56 +0100

    cleanup suggester stories and nomenclatures

--- src/stories/utils/referentiel.js
+++ src/stories/utils/referentiel.js
@@ -1,13 +1,7 @@
 export const getReferentiel = async (name) => {
-	switch (name) {
-		case 'libelle-pcs2020':
-			return fetch('./libelles-pcs-2020.json').then((r) => r.json());
-		case 'naf-rev2-stop':
-		case 'naf-rev2':
-			return fetch('./naf-rev2.json').then((r) => r.json());
-		case 'cog-communes':
-			return fetch('./communes-2019.json').then((r) => r.json());
-		default:
-			throw new Error(`Unknown référentiel ${name}`);
+	 try {
+		return fetch(`./${name}.json`).then((r) => r.json());
+	 } catch (error) {
+		throw new Error(`Unknown référentiel ${name}`);
+	 }
 	}
-};
