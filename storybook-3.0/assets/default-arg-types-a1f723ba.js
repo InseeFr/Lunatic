2651211ebb1e21cd9b215bdd9672f089947bdd98
ddecrulle <ddecrulle@users.noreply.github.com>
2024-03-11 10:42:13 +0000

    deploy: 5da9802a4edbd8c28915f7fdac6912587dc26908

---
+++ storybook-3.0/assets/default-arg-types-a1f723ba.js
@@ -0,0 +1,1 @@
+const a=["source","id","pagination","data","management","activeControls","features","initialPage","getStoreInfo","missing","shortcut","filterDescription"],i=a.reduce((e,t)=>({...e,[t]:{table:{disable:!0}}}),{});export{i as d};
---
+++ storybook-3.0/assets/fakeReferentiel-f4ae79eb.js
@@ -0,0 +1,1 @@
+const e=[{id:"brosse",label:"Brosse à cheveux",price:20},{id:"balle",label:"Balle rebondissante",price:10}];export{e as default};
