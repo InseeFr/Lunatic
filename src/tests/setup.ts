5ae57aaaff4f3fabf35253301116b54f6ba43f51
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2024-04-18 16:26:49 +0200

    fmt

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -2,7 +2,6 @@ import '@testing-library/jest-dom';
 import { afterEach, vi } from 'vitest';
 import { cleanup } from '@testing-library/react';
 
-
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
 	cleanup();
