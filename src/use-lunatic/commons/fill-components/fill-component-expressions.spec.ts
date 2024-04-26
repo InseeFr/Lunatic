f1bbcc19ba3ef8165536ef2b851c2cc88a5c0568
Jonathan <contact@grafikart.fr>
2024-04-26 19:41:48 +0200

    ci: it import missing

--- src/use-lunatic/commons/fill-components/fill-component-expressions.spec.ts
+++ src/use-lunatic/commons/fill-components/fill-component-expressions.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { fillComponentExpressions } from './fill-component-expressions';
 
 describe('fillComponentExpressions', () => {
