5c1acbb42d2d15a07749d2f5f498a437890d3283
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2024-05-15 16:15:43 +0200

    remove generateUniqueId

--- src/components/shared/MDLabel/MarkdownLink.tsx
+++ src/components/shared/MDLabel/MarkdownLink.tsx
@@ -1,4 +1,4 @@
-import { useState, type PropsWithChildren, type ReactNode } from 'react';
+import { useId, type PropsWithChildren, type ReactNode } from 'react';
 import { Tooltip } from 'react-tooltip';
 import { RouterLink } from './RouterLink';
 import { slottableComponent } from '../HOC/slottableComponent';
@@ -8,8 +8,7 @@ type Props = PropsWithChildren<{ href: string; tooltip?: ReactNode }>;
 export const MarkdownLink = slottableComponent<Props>(
 	'MarkdownLink',
 	({ href, children, tooltip }) => {
-		// Must be replace by useId when queen move to React 18.
-		const [id] = useState(() => generateUniqueId());
+		const id = useId();
 
 		const extraProps = tooltip
 			? { 'data-tooltip-id': `tooltip-${id}`, className: 'link-md' }
@@ -42,9 +41,3 @@ export const MarkdownLink = slottableComponent<Props>(
 		);
 	}
 );
-
-export function generateUniqueId(): string {
-	const timestamp = Date.now().toString(36);
-	const randomPart = Math.random().toString(36).substring(2, 8);
-	return `${timestamp}-${randomPart}`;
-}
