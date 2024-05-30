0569221e8c2c5a1422d37e7cc3d2f486da4c9354
Genevois Renaud <KQHLZ2@ad.insee.intra>
2024-05-30 13:46:48 +0200

    story remote component for rp

--- src/stories/RemoteComponent/mock-server.js
+++ src/stories/RemoteComponent/mock-server.js
@@ -9,9 +9,20 @@ app.use(bodyParser.json());
 
 app.post('/api/recensement/adresse', (request, response) => {
 	console.log('body', request.body); // on n'en fait rien, c juste un mock !
-	response
-		.status(201)
-		.json({ NUMVOIE: '14', LIBVOIE: 'DE-PICPUS', TYPEVOIE: 'RUE' });
+	const { ZC, ADR_RANG, LOG_RANG } = request.body;
+	if (ZC && ADR_RANG && LOG_RANG) {
+		response.status(201).json({
+			NUMVOI_LOC: '14',
+			BISTER_LOC: null,
+			TYPEVOI_LOC: 'Rue',
+			NOMVOI_LOC: 'de Picpus',
+			CPOST_LOC: '75012',
+			LIBELLE_COMMUNE: 'Paris',
+			SUGGESTED: true,
+		});
+	} else {
+		response.status(201).json({ SUGGESTED: false });
+	}
 });
 
 app.listen(port, () => {
