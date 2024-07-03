e3549b8676c07a2bf4ca72df23d5a085a1e197f7
Dylan Decrulle <dylan.decrulle@outlook.fr>
2024-07-03 17:16:15 +0200

    remove unused import

--- src/components/Roundabout/roundabout.spec.tsx
+++ src/components/Roundabout/roundabout.spec.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import { CustomRoundabout } from './CustomRoundabout';
 import { describe, it, expect, vi, afterEach } from 'vitest';
