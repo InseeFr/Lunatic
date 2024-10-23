2b17f7104b36522780f200fff15a4f45bed4e5cf
Quentin Ruhier <99256289+QRuhier@users.noreply.github.com>
2024-10-23 15:29:59 +0200

    fix: display description on subsequence (#1162)

--- src/components/Subsequence/Subsequence.tsx
+++ src/components/Subsequence/Subsequence.tsx
@@ -1,10 +1,12 @@
-import { Declarations } from '../shared/Declarations/Declarations';
 import type { LunaticComponentProps } from '../type';
+import { Declarations } from '../shared/Declarations/Declarations';
 import { slottableComponent } from '../shared/HOC/slottableComponent';
+import { LabelDescription } from '../shared/LabelDescription';
 
 function LunaticSubsequence({
 	id,
 	label,
+	description,
 	declarations,
 }: LunaticComponentProps<'Subsequence'>) {
 	return (
@@ -16,6 +18,7 @@ function LunaticSubsequence({
 			>
 				{label}
 			</div>
+			<LabelDescription value={description} />
 			<Declarations
 				type="AFTER_QUESTION_TEXT"
 				declarations={declarations}
