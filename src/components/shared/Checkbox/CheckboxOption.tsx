7d57ec87e592b97ac99493ee16b9aa5919c10be1
Quentin Ruhier <quentin.ruhier@insee.fr>
2024-12-10 16:38:19 +0100

    hotfix: checkboxGroup detail position

--- src/components/shared/Checkbox/CheckboxOption.tsx
+++ src/components/shared/Checkbox/CheckboxOption.tsx
@@ -65,39 +65,41 @@ function LunaticCheckboxOption({
 	);
 
 	return (
-		<div
-			id={id}
-			role="checkbox"
-			aria-invalid={invalid}
-			aria-disabled={disabled}
-			aria-readonly={readOnly}
-			className="lunatic-input-checkbox"
-			aria-checked={checked}
-			tabIndex={0}
-			onClick={onClickOption}
-			onKeyDown={handleKeyDown}
-			aria-labelledby={labelId}
-		>
-			<div className="lunatic-input-checkbox__icon">
-				{checked && (
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 14 11"
-					>
-						<path
-							d="M5 11 0 6l1.4-1.4L5 8.2 12.6.6 14 2l-9 9Z"
-							fill="currentColor"
-						/>
-					</svg>
-				)}
+		<>
+			<div
+				id={id}
+				role="checkbox"
+				aria-invalid={invalid}
+				aria-disabled={disabled}
+				aria-readonly={readOnly}
+				className="lunatic-input-checkbox"
+				aria-checked={checked}
+				tabIndex={0}
+				onClick={onClickOption}
+				onKeyDown={handleKeyDown}
+				aria-labelledby={labelId}
+			>
+				<div className="lunatic-input-checkbox__icon">
+					{checked && (
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 14 11"
+						>
+							<path
+								d="M5 11 0 6l1.4-1.4L5 8.2 12.6.6 14 2l-9 9Z"
+								fill="currentColor"
+							/>
+						</svg>
+					)}
+				</div>
+				<Label id={labelId} htmlFor={id} description={description}>
+					{codeModality && (
+						<span className="code-modality">{codeModality.toUpperCase()}</span>
+					)}{' '}
+					{label}
+				</Label>
 			</div>
-			<Label id={labelId} htmlFor={id} description={description}>
-				{codeModality && (
-					<span className="code-modality">{codeModality.toUpperCase()}</span>
-				)}{' '}
-				{label}
-			</Label>
 			{hasDetail && (checked || detailAlwaysDisplayed) && (
 				<CustomInput
 					id="detailId"
@@ -107,7 +109,7 @@ function LunaticCheckboxOption({
 					disabled={disabled}
 				/>
 			)}
-		</div>
+		</>
 	);
 }
 
