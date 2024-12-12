225fbb989b51a970b4ae93221c2e69429a0be9ed
Chloe Renaud <chloe.renaud@insee.fr>
2024-12-12 15:09:34 +0100

    chore: github pages deployment

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
 	url: 'https://inseefr.github.io/',
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: 'Lunatic/docs',
+	baseUrl: '/lunatic/',
 	plugins: [
 		'./src/plugins/lunaticFixesPlugin.ts',
 		[require.resolve('@cmfcmf/docusaurus-search-local'), { indexBlog: false }],
