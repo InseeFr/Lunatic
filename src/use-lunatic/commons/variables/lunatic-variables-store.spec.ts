583c5dd222d740b324e2c142b86bac56dd395908
Laurent Caouissin <caouissin.laurent@gmail.com>
2025-03-19 16:06:11 +0100

    chore: fix typing

--- src/use-lunatic/commons/variables/lunatic-variables-store.spec.ts
+++ src/use-lunatic/commons/variables/lunatic-variables-store.spec.ts
@@ -510,10 +510,6 @@ describe('lunatic-variables-store', () => {
 						{
 							name: 'PRENOM',
 							values: {
-								EDITED: null,
-								FORCED: null,
-								INPUTTED: null,
-								PREVIOUS: null,
 								COLLECTED: 'John',
 							},
 							variableType: 'COLLECTED',
@@ -521,10 +517,6 @@ describe('lunatic-variables-store', () => {
 						{
 							name: 'NOM',
 							values: {
-								EDITED: null,
-								FORCED: null,
-								INPUTTED: null,
-								PREVIOUS: null,
 								COLLECTED: '',
 							},
 							variableType: 'COLLECTED',
