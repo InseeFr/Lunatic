6712cc8643b66bffe920623e64c6518190914c16
Mailine Nguyen <nguyen.mailine@gmail.com>
2025-04-11 12:15:25 +0200

    chore: format

--- src/components/FilterDescription/FilterDescription.tsx
+++ src/components/FilterDescription/FilterDescription.tsx
@@ -1,35 +1,28 @@
-
 import { slottableComponent } from '../shared/HOC/slottableComponent';
 import { LunaticComponentProps } from '../type';
 
-type Props = LunaticComponentProps<'FilterDescription'>
+type Props = LunaticComponentProps<'FilterDescription'>;
 
 export function FilterDescription({
 	id,
 	label,
 }: LunaticComponentProps<'FilterDescription'>) {
-	return (
-		<CustomFilterDescription
-			id={id}
-			label={label} />
-	)
+	return <CustomFilterDescription id={id} label={label} />;
 }
 
-export const CustomFilterDescription = slottableComponent<Props>('FilterDescription', (props) => {
-	const {
-		id,
-		label,
-	} = props;
+export const CustomFilterDescription = slottableComponent<Props>(
+	'FilterDescription',
+	(props) => {
+		const { id, label } = props;
 
-	return (
-		<div
-			id={`filter-description-${id}`}
-			aria-label={`filter-description`}
-			className="filter-description-lunatic"
-		>
-			{label}
-		</div>
-	)
-}
-)
-
+		return (
+			<div
+				id={`filter-description-${id}`}
+				aria-label={`filter-description`}
+				className="filter-description-lunatic"
+			>
+				{label}
+			</div>
+		);
+	}
+);
