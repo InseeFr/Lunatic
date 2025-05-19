041c8c7b5be12813ff08b3712fb1369c11ce3cae
Chloe Renaud <chloe.renaud@insee.fr>
2025-05-19 10:33:43 +0200

    feat: add 'MANDATORY' type to controls

--- src/type.source.ts
+++ src/type.source.ts
@@ -377,7 +377,7 @@ export type VTLScalarExpression = {
 export type ControlDefinition = {
 	id: string;
 	criticality: 'INFO' | 'WARN' | 'ERROR';
-	typeOfControl?: 'FORMAT' | 'CONSISTENCY';
+	typeOfControl?: 'FORMAT' | 'CONSISTENCY' | 'MANDATORY';
 	control: VTLExpression;
 	errorMessage: VTLExpression;
 	bindingDependencies?: string[];
