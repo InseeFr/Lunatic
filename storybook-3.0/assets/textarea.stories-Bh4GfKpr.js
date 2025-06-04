6533892ec9effe9b2256f53bd21c0dbed697e670
QRuhier <QRuhier@users.noreply.github.com>
2025-06-04 15:00:14 +0000

    deploy: a481e96777a5833565b38c642934ecbc22319c83

---
+++ storybook-3.0/assets/textarea.stories-Bh4GfKpr.js
@@ -0,0 +1,5 @@
+import{O as r}from"./Orchestrator-D56RHRaz.js";import"./jsx-runtime-BlAj40OV.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Combobox-CPxcyoe5.js";import"./index-Cf-03bMR.js";const n=[{page:"1",id:"name",componentType:"Textarea",label:{value:'"Label"',type:"VTL|MD"},description:{value:'"Description"',type:"VTL|MD"},conditionFilter:{value:"true",type:"VTL"},maxLength:30,response:{name:"SOMETHING"}}],s=[{variableType:"COLLECTED",name:"SOMETHING",componentRef:"name",values:{COLLECTED:null}}],p={components:n,variables:s},d={title:"Components/Textarea",...r},e={args:{source:p}};var t,a,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    source
+  }
+}`,...(o=(a=e.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const L=["Default"];export{e as Default,L as __namedExportsOrder,d as default};
