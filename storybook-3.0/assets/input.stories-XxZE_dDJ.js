f81862afda5a6826c6694f259056f4c8c0fbfe81
laurentC35 <laurentC35@users.noreply.github.com>
2025-09-01 14:44:57 +0000

    deploy: 0ecca62c4e9b02a260fdaf02c662ae3788973a81

---
+++ storybook-3.0/assets/input.stories-XxZE_dDJ.js
@@ -0,0 +1,5 @@
+import{a as s}from"./Orchestrator-DKX7VZwt.js";import{s as a}from"./source-BCE_ZAxJ.js";import"./jsx-runtime-BlAj40OV.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Combobox-C6KMZboJ.js";import"./index-Cf-03bMR.js";import"./index-Dk74W0Oi.js";const l={title:"Components/Input",...s},r={args:{source:a}};var t,o,e;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    source
+  }
+}`,...(e=(o=r.parameters)==null?void 0:o.docs)==null?void 0:e.source}}};const g=["Default"];export{r as Default,g as __namedExportsOrder,l as default};
---
+++ storybook-3.0/assets/slots.stories-DwvOBb9o.js
@@ -0,0 +1,15 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{a as c}from"./Orchestrator-DKX7VZwt.js";import{s as u}from"./source-BCE_ZAxJ.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Combobox-C6KMZboJ.js";import"./index-Cf-03bMR.js";import"./index-Dk74W0Oi.js";const x={title:"Behaviour/Slots",...c},a={args:{source:u,slots:{Input:({onChange:o,value:l,label:n})=>e.jsxs("label",{className:"floating-label",children:[e.jsx("input",{placeholder:"Nom de l'occupant",className:"input",value:l??"",onChange:p=>o(p.currentTarget.value)}),e.jsx("span",{children:n})]})}}};var r,s,t;a.parameters={...a.parameters,docs:{...(r=a.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    source,
+    slots: {
+      Input: ({
+        onChange,
+        value,
+        label
+      }) => <label className="floating-label">
+                    <input placeholder="Nom de l'occupant" className="input" value={value ?? ''} onChange={e => onChange(e.currentTarget.value)} />
+                    <span>{label}</span>
+                </label>
+    }
+  }
+}`,...(t=(s=a.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const N=["Default"];export{a as Default,N as __namedExportsOrder,x as default};
