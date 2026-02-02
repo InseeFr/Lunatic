ee8a0b94bc3618bd2119ba900bcdbbc801070b97
laurentC35 <laurentC35@users.noreply.github.com>
2026-02-02 08:45:20 +0000

    deploy: d3f7dad9b02ce8539dc9e6ba2f42db19297ce6a5

--- storybook-3.0/assets/multimode.stories-Cyi3Fbod.js
+++
@@ -1,6 +0,0 @@
-import{s as a}from"./roundabout-Dc260yZ1.js";import{O as s}from"./Orchestrator-CebZC4LN.js";import"./jsx-runtime-BjG_zV1W.js";import"./Combobox-B46hElJN.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-xW6owYLq.js";import"./index-BL20XVM2.js";import"./index-Dk74W0Oi.js";const f={title:"Behaviour/Articulation/MultiMode",component:s},r={args:{source:a,multiMode:!0}};var t,o,e;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    source,
-    multiMode: true
-  }
-}`,...(e=(o=r.parameters)==null?void 0:o.docs)==null?void 0:e.source}}};const M=["Default"];export{r as Default,M as __namedExportsOrder,f as default};
