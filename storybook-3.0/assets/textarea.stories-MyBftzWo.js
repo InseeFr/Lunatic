74286ee8b3c16081fdd9e80da6fbeab942589fcf
laurentC35 <laurentC35@users.noreply.github.com>
2026-04-10 13:26:23 +0000

    deploy: 50d4a5de16805956dd025464a6456866b515d521

--- storybook-3.0/assets/textarea.stories-MyBftzWo.js
+++
@@ -1,5 +0,0 @@
-import{a as r}from"./Orchestrator-4684baMv.js";import"./jsx-runtime-BjG_zV1W.js";import"./Combobox-BMViUnsY.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-xW6owYLq.js";import"./index-BL20XVM2.js";import"./index-Dk74W0Oi.js";const n=[{page:"1",id:"name",componentType:"Textarea",label:{value:'"Label"',type:"VTL|MD"},description:{value:'"Description"',type:"VTL|MD"},conditionFilter:{value:"true",type:"VTL"},maxLength:30,response:{name:"SOMETHING"}}],s=[{variableType:"COLLECTED",name:"SOMETHING",componentRef:"name",values:{COLLECTED:null}}],p={components:n,variables:s},L={title:"Components/Textarea",...r},e={args:{source:p}};var t,a,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    source
-  }
-}`,...(o=(a=e.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const v=["Default"];export{e as Default,v as __namedExportsOrder,L as default};
